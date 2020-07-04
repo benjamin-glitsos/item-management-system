b9c9ceef21a86d73ad77afcc9b116804ce2b26fd
Benjamin Glitsos <bengyup@gmail.com>
2020-07-04 16:19:44 +1000

    started a scripts file for commonly-used scripts in this project

---
+++ scripts.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+for arg in "$@"
+do
+    case $arg in
+        "run" )
+            sudo docker-compose up
+        ;;
+        * )
+            echo "Open this file in a text editor to view the shell scripts that are commonly used for this project"
+        ;;
+    esac
+done
