dee046cb19746adff208a17af129e687e0b4a2d0
Benjamin Glitsos <bengyup@gmail.com>
2020-07-04 16:22:09 +1000

    added a new script

--- scripts.sh
+++ scripts.sh
@@ -6,6 +6,9 @@ do
         "run" )
             sudo docker-compose up
         ;;
+        "prune" )
+            sudo docker container prune
+        ;;
         * )
             echo "Open this file in a text editor to view the shell scripts that are commonly used for this project"
         ;;
