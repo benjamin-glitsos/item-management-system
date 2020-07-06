029df3ef18db63b0cdfc099e9d50057887c2b2df
Benjamin Glitsos <bengyup@gmail.com>
2020-07-06 19:30:44 +1000

    added all the docker runs and builds for the microservices to the scripts file

--- scripts.sh
+++ scripts.sh
@@ -15,6 +15,30 @@ do
         "containers" )
             docker container ls -a
         ;;
+        "build app" )
+            docker build -t hospital_app ./app/
+        ;;
+        "run app" )
+            docker run -it hospital_app
+        ;;
+        "build admin" )
+            docker build -t hospital_admin ./admin/
+        ;;
+        "run admin" )
+            docker run -it hospital_admin
+        ;;
+        "build database" )
+            docker build -t hospital_database ./database/
+        ;;
+        "run database" )
+            docker run -it hospital_database
+        ;;
+        "build database_admin" )
+            docker build -t hospital_database_admin ./database-admin/
+        ;;
+        "run database_admin" )
+            docker run -it hospital_database_admin
+        ;;
         * )
             cat ./scripts.sh
         ;;
