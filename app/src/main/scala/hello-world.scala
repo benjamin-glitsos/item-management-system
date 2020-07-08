02c581a869555ba9f115ac1989a0aaf49d7038ac
Benjamin Glitsos <bengyup@gmail.com>
2020-07-08 21:58:34 +1000

    now starting to add some other dependencies to then start coding with

--- app/src/main/scala/hello-world.scala
+++ app/src/main/scala/hello-world.scala
@@ -1,12 +1,5 @@
-import cats._
-import cats.data._
-import cats.implicits._
-
-// def sum(xs: List[Int]): Int = xs.foldLeft(0) { _ + _ }
-// println(sum(List(1, 2, 3, 4)))
-
 object Hello {
     def main(args: Array[String]) = {
-        println("Hello, " |+| "World!")
+        println("Hello world")
     }
 }
