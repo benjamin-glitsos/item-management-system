86df6e7706060108596adb6e9e85b8fbfed895d2
Benjamin Glitsos <bengyup@gmail.com>
2020-07-12 19:58:05 +1000

    defined a User type in order to make code more DRY

--- app/src/main/scala/database/Users.scala
+++ app/src/main/scala/database/Users.scala
@@ -3,23 +3,25 @@ import slick.jdbc.meta.MTable
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class UsersTable(tag: Tag) extends Table[(Int, String, String)](tag, "users") {
-    def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
-
-    def username = column[String]("username")
-
-    def password = column[String]("password")
-
-    def * = (id, username, password)
-}
-
 object Users extends Seeder {
+    type User = (Int, String, String)
+
+    class UsersTable(tag: Tag) extends Table[User](tag, "users") {
+        def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+
+        def username = column[String]("username")
+
+        def password = column[String]("password")
+
+        def * = (id, username, password)
+    }
+
     val users = TableQuery[UsersTable]
 
     def initialise() = DBIO.seq(
         users.schema.drop,
         users.schema.create,
-        users ++= seed[(Int, String, String)](
+        users ++= seed[User](
             1,
             10,
             (0, newPerson().getUsername(), newPerson().getPassword())
