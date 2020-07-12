2007ceec1f82102958d899931d7bbf112b4855a5
Benjamin Glitsos <bengyup@gmail.com>
2020-07-12 14:02:34 +1000

    JDBC url when in config file not working

--- app/src/main/scala/HelloWorld.scala
+++ app/src/main/scala/HelloWorld.scala
@@ -15,19 +15,16 @@ class Users(tag: Tag) extends Table[(Int, String)](tag, "USERS") {
 object HelloWorld {
     val person: Person = Fairy.create().person()
 
-    val db = Database.forURL(
-        s"jdbc:postgresql://${System.getenv("POSTGRES_HOST")}/${System.getenv("POSTGRES_DB")}",
-        driver = "org.postgresql.Driver",
-        user = System.getenv("POSTGRES_USER"),
-        password = System.getenv("POSTGRES_PASSWORD")
-    )
+    val db = Database.forConfig("database")
 
     val users = TableQuery[Users]
 
     val queries = DBIO.seq(
       users.schema.create,
       users += (1, "aaaa"),
-      users += (2, "bbbb")
+      users += (2, "bbbb"),
+      users += (2, "cccc"),
+      users += (2, "dddd")
     )
 
     def main(args: Array[String]) = {
