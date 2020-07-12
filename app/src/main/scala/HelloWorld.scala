c52b365ff641a00406221373bf898ffa022e49fc
Benjamin Glitsos <bengyup@gmail.com>
2020-07-12 10:39:32 +1000

    started trying to use jfairy java library

--- app/src/main/scala/HelloWorld.scala
+++ app/src/main/scala/HelloWorld.scala
@@ -1,5 +1,9 @@
+import com.devskiller.jfairy.Fairy
+import com.devskiller.jfairy.producer.person.Person
+
 object HelloWorld {
+    val person: Person = Fairy.create().person()
     def main(args: Array[String]) = {
-        println("Hello world")
+        println(person)
     }
 }
