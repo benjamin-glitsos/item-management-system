fa89d602e38a4fc6406f2e5d384738422336b228
Benjamin Glitsos <bengyup@gmail.com>
2020-07-13 20:17:45 +1000

    test foreign key works

--- app/src/main/scala/database/Users.scala
+++ app/src/main/scala/database/Users.scala
@@ -6,7 +6,7 @@ import eu.timepit.refined.auto._
 import eu.timepit.refined.numeric._
 
 object Users extends Seeder {
-    type User = (Int, String, String)
+    type User = (Int, String, String, Int)
 
     val seedCount: Int Refined Positive = 4
 
@@ -14,17 +14,19 @@ object Users extends Seeder {
         def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
         def username = column[String]("username")
         def password = column[String]("password")
-        def * = (id, username, password)
+        def records_id = column[Int]("records_id")
+        def * = (id, username, password, records_id)
+        def records_fk = foreignKey("results_fk", id, Records.records)(_.id)
     }
 
     val users = TableQuery[UsersTable]
 
     def initialise() = DBIO.seq(
-        users.schema.drop,
+        // users.schema.drop,
         users.schema.create,
         users ++= seed[User](
             seedCount,
-            (0, newPerson().getUsername(), newPerson().getPassword())
+            (0, newPerson().getUsername(), newPerson().getPassword(), 1)
         )
     )
 }
