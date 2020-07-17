4255c812b5272f4504ea2c03f5f260f8e7de2e43
Benjamin Glitsos <bengyup@gmail.com>
2020-07-17 21:39:49 +1000

    fixed oversight of not having db.run

--- app/src/main/scala/data/Data.scala
+++ app/src/main/scala/data/Data.scala
@@ -6,54 +6,56 @@ object Data extends Connection with Seeder {
     val schema = RecordsDAO.schema ++ PeopleDAO.schema ++ UsersDAO.schema
 
     def setup() = {
-        DBIO.seq(
-            // Reset database schema to blank state
-            sqlu"DROP SCHEMA public CASCADE",
-            sqlu"CREATE SCHEMA public",
-            sqlu"GRANT ALL ON SCHEMA public TO postgres",
-            sqlu"GRANT ALL ON SCHEMA public TO public",
+        db.run(
+            DBIO.seq(
+                // Reset database schema to blank state
+                sqlu"DROP SCHEMA public CASCADE",
+                sqlu"CREATE SCHEMA public",
+                sqlu"GRANT ALL ON SCHEMA public TO postgres",
+                sqlu"GRANT ALL ON SCHEMA public TO public",
 
-            // Create schema
-            schema.create,
+                // Create schema
+                schema.create,
 
-            // Seed tables with fake data
-            RecordsDAO ++= seed[Record](
-                RecordsDAO.seedCount,
-                (
-                    id,
-                    Some(new Timestamp(System.currentTimeMillis())),
-                    Some(randFK(UsersDAO.seedCount)),
-                    Some(new Timestamp(System.currentTimeMillis())),
-                    Some(randFK(UsersDAO.seedCount)),
-                    Some(new Timestamp(System.currentTimeMillis())),
-                    Some(randFK(UsersDAO.seedCount))
+                // Seed tables with fake data
+                RecordsDAO ++= seed[Record](
+                    RecordsDAO.seedCount,
+                    (
+                        id,
+                        Some(new Timestamp(System.currentTimeMillis())),
+                        Some(randFK(UsersDAO.seedCount)),
+                        Some(new Timestamp(System.currentTimeMillis())),
+                        Some(randFK(UsersDAO.seedCount)),
+                        Some(new Timestamp(System.currentTimeMillis())),
+                        Some(randFK(UsersDAO.seedCount))
+                    )
+                ),
+                PeopleDAO ++= seed[Person](
+                    PeopleDAO.seedCount,
+                    (
+                        id,
+                        randFK(RecordsDAO.seedCount),
+                        newPerson().getFirstName(),
+                        newPerson().getLastName(),
+                        Some(newPerson().getMiddleName()),
+                        newPerson().getEmail(),
+                        newPerson().getTelephoneNumber(),
+                        newPerson().getAddress().getAddressLine1(),
+                        Some(newPerson().getAddress().getAddressLine2()),
+                        "NSW",
+                        newPerson().getAddress().getPostalCode()
+                    )
+                ),
+                UsersDAO ++= seed[User](
+                    UsersDAO.seedCount,
+                    (
+                        id,
+                        randFK(PeopleDAO.seedCount),
+                        newPerson().getUsername(),
+                        newPerson().getPassword()
+                    )
                 )
-            ),
-            PeopleDAO ++= seed[Person](
-                PeopleDAO.seedCount,
-                (
-                    id,
-                    randFK(RecordsDAO.seedCount),
-                    newPerson().getFirstName(),
-                    newPerson().getLastName(),
-                    Some(newPerson().getMiddleName()),
-                    newPerson().getEmail(),
-                    newPerson().getTelephoneNumber(),
-                    newPerson().getAddress().getAddressLine1(),
-                    Some(newPerson().getAddress().getAddressLine2()),
-                    "NSW",
-                    newPerson().getAddress().getPostalCode()
-                )
-            ),
-            UsersDAO ++= seed[User](
-                UsersDAO.seedCount,
-                (
-                    id,
-                    randFK(PeopleDAO.seedCount),
-                    newPerson().getUsername(),
-                    newPerson().getPassword()
-                )
-            ),
+            )
         )
     }
 }
