83bfb4b48a48243d1f3bc6a880b3ddb0bb2a588b
Benjamin Glitsos <bengyup@gmail.com>
2020-07-20 19:03:54 +1000

    using http4s.circe.CirceEntityEncoder to automatically convert all API routes to json

--- app/src/main/scala/routes/UsersRoute.scala
+++ app/src/main/scala/routes/UsersRoute.scala
@@ -4,7 +4,7 @@ import org.http4s.HttpRoutes
 import org.http4s.syntax._
 import org.http4s.dsl.io._
 import org.http4s.implicits._
-// import org.http4s.circe.CirceEntityEncoder._
+import org.http4s.circe.CirceEntityEncoder._
 import org.http4s.circe._
 import io.circe.syntax._
 import io.circe.generic.auto._
@@ -18,7 +18,7 @@ object UsersRoute {
 
     def service = HttpRoutes.of[IO] {
         case GET -> Root =>
-            Ok(IO.fromFuture(IO(UsersDAO.list)).map((x: Seq[User]) => (x.asJson)))
+            Ok(IO.fromFuture(IO(UsersDAO.list)))
     }.orNotFound
 }
 
@@ -27,24 +27,3 @@ object UsersRoute {
 // users?page=1&sort.username=desc&search.username=lorem
 // users/1
 // users/1?tab=person
-
-// case GET -> Root => UsersDAO.all.flatMap(_.fold(NotFound())(Ok(_.asJson)))
-
-// case GET -> Root => UsersDAO.all onComplete {
-//     case Success(x) => Ok(x.asJson)
-//     case Failure(x) => NotFound()
-// }
-
-// case GET -> Root => Ok(IO.fromFuture(IO(Future {
-//     println("I run when the future is constructed.")
-//     "Greetings from the future!"
-// })))
-// UsersDAO.list.map(_.asJson)
-
-// .flatMap {
-//   x => Ok(x)
-// }
-// .handleWith {
-//   case CustomNotFoundException(reason) => NotFound(reason)
-//   case CustomConflictException() => Conflict()
-// }
