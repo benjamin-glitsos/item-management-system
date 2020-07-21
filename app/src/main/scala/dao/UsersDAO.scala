59841b583c286c8a86edef65e336ada5f7f9ece9
Benjamin Glitsos <bengyup@gmail.com>
2020-07-21 20:05:45 +1000

    the show query is compiling but not yet working

--- app/src/main/scala/dao/UsersDAO.scala
+++ app/src/main/scala/dao/UsersDAO.scala
@@ -1,13 +1,15 @@
-import scala.concurrent.Future
+import scala.concurrent._
 import slick.jdbc.PostgresProfile.api._
 
 object UsersDAO extends TableQuery(new UsersSchema(_)) with Connection {
+    implicit val futureExecutionContext: ExecutionContext = ExecutionContext.global
+
     def list(): Future[Seq[User]] = {
         db.run(this.result)
     }
 
-    def show(id: Int): Future[Seq[User]] = {
-        db.run(this.filter(_.id === id).result)
+    def show(id: Int): Future[Option[User]] = {
+        db.run(this.filter(_.id === id).result).map(_.headOption)
     }
 
     def delete(id: Int): Future[Int] = {
--- app/src/main/scala/routes/UsersRoutes.scala
+++ app/src/main/scala/routes/UsersRoutes.scala
@@ -1,25 +1,22 @@
 import cats.effect._
 import org.http4s.HttpRoutes
 import org.http4s.dsl.io._
-import org.http4s.implicits._
 import org.http4s.circe.CirceEntityEncoder._
 import io.circe.generic.auto._
 import scala.concurrent._
-import org.http4s.server.Router
 
 object UsersRoutes {
     implicit val contextShift: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
 
     object Id extends QueryParamDecoderMatcher[Int]("id")
-    object MaybeId extends OptionalQueryParamDecoderMatcher[Int]("id")
+    object MaybeId extends OptionalQueryParamDecoderMatcher[Int]("maybeId")
 
     val routes = HttpRoutes.of[IO] {
         case GET -> Root :? MaybeId(maybeId) =>
             maybeId match {
               case None =>
                 Ok(IO.fromFuture(IO(UsersDAO.list)))
-              case Some(id) =>
-                Ok(IO.fromFuture(IO(UsersDAO.show(id))))
+              case Some(id) => IO.fromFuture(IO(UsersDAO.show(id))).flatMap(_.fold(NotFound())(Ok(_)))
             }
         case DELETE -> Root :? Id(id) =>
             Ok(IO.fromFuture(IO(UsersDAO.delete(id))))
