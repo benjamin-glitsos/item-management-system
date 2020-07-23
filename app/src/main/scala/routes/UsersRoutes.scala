4c723949adbaab77193dc3ac9656c06973ebe1df
Benjamin Glitsos <bengyup@gmail.com>
2020-07-23 21:32:27 +1000

    fixed an issue caused by syntax

--- app/src/main/scala/routes/UsersRoutes.scala
+++ app/src/main/scala/routes/UsersRoutes.scala
@@ -29,12 +29,9 @@ object UsersRoutes {
                     Ok(IO.fromFuture(IO(UsersDAO.list(rows, page))))
                 case Some(id) =>
                     IO.fromFuture(IO(UsersDAO.show(id))).flatMap(_.fold(NotFound())(Ok(_)))
-                    // TODO: split this into separate tabs by a url parameter. default = first tab
             }
+            case DELETE -> Root :? Id(id) =>
+                Ok(IO.fromFuture(IO(UsersDAO.delete(id))))
         }
-                case DELETE -> Root :? Id(id) =>
-                    Ok(IO.fromFuture(IO(UsersDAO.delete(id))))
     }
 }
-
-// TODO: join: records -> people -> users. Then later the first two can be abstracted out and applied to many queries
