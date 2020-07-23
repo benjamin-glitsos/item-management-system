df1aa81ffd7dec55004a279e7dc085a8a0b599db
Benjamin Glitsos <bengyup@gmail.com>
2020-07-23 18:23:40 +1000

    added some todos

--- app/src/main/scala/loaders/Loader.scala
+++ app/src/main/scala/loaders/Loader.scala
@@ -20,6 +20,8 @@ object Loader extends Connection {
                 RecordsDAO += RecordsLoader.blank,
                 PeopleDAO += PeopleLoader.admin,
                 UsersDAO += UsersLoader.admin,
+                // TODO: use the API endpoints to insert the data
+                // TODO: use the endpoints to insert data into all tables in the correct order and linking to each other then remove seedCount values?
                 RecordsDAO.filter(_.id === 1).update(RecordsLoader.admin),
 
                 // Seed tables with randomised fake data
