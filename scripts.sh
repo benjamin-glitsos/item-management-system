e1ef28627c6e9bd99dd0e3fe27f77de68aede3f7
Benjamin Glitsos <bengyup@gmail.com>
2020-07-26 11:21:56 +1000

    started HMS-1 at same time as HMS-2 and HMS-3 are in progress

--- scripts.sh
+++ scripts.sh
@@ -10,36 +10,36 @@ case "$@" in
     "containers" )
         docker container ls -a
     ;;
-    "build app" )
-        docker build -t hospital_app ./app/
-    ;;
-    "run app" )
-        docker run \
-            -it \
-            --volume=app:/root \
-            --volume=$HOME/Documents/code/hospital-ms/app:/usr/src/app \
-            --env-file .env \
-            -p 3100:3100 \
-            hospital_app
-    ;;
-    "build admin" )
-        docker build -t hospital_admin ./admin/
-    ;;
-    "run admin" )
-        docker run -it hospital_admin
-    ;;
-    "build database" )
-        docker build -t hospital_database ./database/
-    ;;
-    "run database" )
-        docker run -it hospital_database
-    ;;
-    "build database_admin" )
-        docker build -t hospital_database_admin ./database-admin/
-    ;;
-    "run database_admin" )
-        docker run -it hospital_database_admin
-    ;;
+    # "build app" )
+    #     docker build -t hospital_app ./app/
+    # ;;
+    # "run app" )
+    #     docker run \
+    #         -it \
+    #         --volume=app:/root \
+    #         --volume=$HOME/Documents/code/hospital-ms/app:/usr/src/app \
+    #         --env-file .env \
+    #         -p 3100:3100 \
+    #         hospital_app
+    # ;;
+    # "build admin" )
+    #     docker build -t hospital_admin ./admin/
+    # ;;
+    # "run admin" )
+    #     docker run -it hospital_admin
+    # ;;
+    # "build database" )
+    #     docker build -t hospital_database ./database/
+    # ;;
+    # "run database" )
+    #     docker run -it hospital_database
+    # ;;
+    # "build database_admin" )
+    #     docker build -t hospital_database_admin ./database-admin/
+    # ;;
+    # "run database_admin" )
+    #     docker run -it hospital_database_admin
+    # ;;
     * )
         cat ./scripts.sh
     ;;
