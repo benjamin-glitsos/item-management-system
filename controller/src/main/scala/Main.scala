5499855329750811b10807217b9916a09a598656
Benjamin Glitsos <bengyup@gmail.com>
2020-08-12 20:34:17 +1000

    abstracted the queries into their own function

--- controller/src/main/scala/Main.scala
+++ controller/src/main/scala/Main.scala
@@ -48,72 +48,89 @@ object Main {
         password: String
     )
 
-    def upsert(record: RecordEdit, person: PersonEdit, user: UserEdit): ConnectionIO[Unit] = {
+
+    def upsertRecord(record: RecordEdit) = {
+        sql"""
+        INSERT INTO records (uuid, created_at, created_by)
+        VALUES (${record.uuid}, NOW(), ${record.user_id})
+        ON CONFLICT (uuid)
+        DO UPDATE SET
+              updated_at = EXCLUDED.created_at
+            , updated_by = EXCLUDED.created_by
+        RETURNING id, updated_by
+        """.query[RecordReturn].unique
+    }
+
+    def insertUser(record_id: Int, person: PersonEdit, user: UserEdit) = {
+        sql"""
+        WITH new_person AS (
+            INSERT INTO people (
+              record_id
+            , first_name
+            , last_name
+            , other_names
+            , sex_id
+            , email_address
+            , phone_number
+            , address_line_one
+            , address_line_two
+            , zip
+            ) VALUES (
+                  ${record_id}
+                , ${person.first_name}
+                , ${person.last_name}
+                , ${person.other_names}
+                , ${person.sex_id}
+                , ${person.email_address}
+                , ${person.phone_number}
+                , ${person.address_line_one}
+                , ${person.address_line_two}
+                , ${person.zip}
+            ) RETURNING id
+        )
+        INSERT INTO users (person_id, username, password)
+        VALUES (
+            (SELECT id FROM new_person)
+          , ${user.username}
+          , ${user.password}
+        )
+        """.update.run
+    }
+
+    def updateUser(record_id: Int, person: PersonEdit, user: UserEdit) = {
+        sql"""
+        WITH existing_person AS (
+            UPDATE people SET
+                first_name       = ${person.first_name}
+              , last_name        = ${person.last_name}
+              , other_names      = ${person.other_names}
+              , sex_id           = ${person.sex_id}
+              , email_address    = ${person.email_address}
+              , phone_number     = ${person.phone_number}
+              , address_line_one = ${person.address_line_one}
+              , address_line_two = ${person.address_line_two}
+              , zip              = ${person.zip}
+            WHERE record_id      = ${record_id}
+            RETURNING id
+        )
+        UPDATE users SET
+            username = ${user.username}
+          , password = ${user.password}
+        WHERE person_id = (SELECT id FROM existing_person)
+        """.update.run
+    }
+
+    def upsert(
+        record: RecordEdit,
+        person: PersonEdit,
+        user: UserEdit
+    ): ConnectionIO[Unit] = {
         for {
-          r <- sql"""
-              INSERT INTO records (uuid, created_at, created_by)
-              VALUES (${record.uuid}, NOW(), ${record.user_id})
-              ON CONFLICT (uuid)
-              DO UPDATE SET
-                    updated_at = EXCLUDED.created_at
-                  , updated_by = EXCLUDED.created_by
-              RETURNING id, updated_by
-              """.query[RecordReturn].unique
+          r <- upsertRecord(record)
           _ <- if (r.updated_by.isEmpty) {
-                  sql"""
-                  WITH new_person AS (
-                      INSERT INTO people (
-                        record_id
-                      , first_name
-                      , last_name
-                      , other_names
-                      , sex_id
-                      , email_address
-                      , phone_number
-                      , address_line_one
-                      , address_line_two
-                      , zip
-                      ) VALUES (
-                            ${r.id}
-                          , ${person.first_name}
-                          , ${person.last_name}
-                          , ${person.other_names}
-                          , ${person.sex_id}
-                          , ${person.email_address}
-                          , ${person.phone_number}
-                          , ${person.address_line_one}
-                          , ${person.address_line_two}
-                          , ${person.zip}
-                      ) RETURNING id
-                  )
-                  INSERT INTO users (person_id, username, password)
-                  VALUES (
-                      (SELECT id FROM new_person)
-                    , ${user.username}
-                    , ${user.password}
-                  )
-                  """.update.run
+                  insertUser(r.id, person, user)
               } else {
-                  sql"""
-                  WITH existing_person AS (
-                      UPDATE people SET
-                          first_name       = ${person.first_name}
-                        , last_name        = ${person.last_name}
-                        , other_names      = ${person.other_names}
-                        , sex_id           = ${person.sex_id}
-                        , email_address    = ${person.email_address}
-                        , phone_number     = ${person.phone_number}
-                        , address_line_one = ${person.address_line_one}
-                        , address_line_two = ${person.address_line_two}
-                        , zip              = ${person.zip}
-                      WHERE record_id      = ${r.id}
-                      RETURNING id
-                  )
-                  UPDATE users SET
-                      username = ${user.username}
-                    , password = ${user.password}
-                  WHERE person_id = (SELECT id FROM existing_person)
-                  """.update.run
+                  updateUser(r.id, person, user)
               }
         } yield ()
     }
