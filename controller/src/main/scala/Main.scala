780536f23dd64d78e2194061440e82b750bb7096
Benjamin Glitsos <bengyup@gmail.com>
2020-08-12 19:42:33 +1000

    some small tidying up of upsert function

--- controller/src/main/scala/Main.scala
+++ controller/src/main/scala/Main.scala
@@ -19,82 +19,83 @@ object Main {
         Blocker.liftExecutionContext(ExecutionContexts.synchronous)
     )
 
-    case class Record2(
+    case class RecordReturn(
         id: Int,
         updated_by: Option[Int],
     )
 
-    val program3: ConnectionIO[Int] =
-      for {
-        record <- sql"""
-            INSERT INTO records (uuid, created_at, created_by)
-            VALUES ('846bc87f-4efe-43cb-9a57-75e20f96db6f', '2020-08-11 08:28:09.517903', 1)
-            ON CONFLICT (uuid)
-            DO UPDATE SET
-                  updated_at = EXCLUDED.created_at
-                , updated_by = EXCLUDED.created_by
-            RETURNING id, updated_by
-            """.query[Record2].unique
-        val isNew = record.updated_by.isDefined
-        _ <- if (isNew) {
-                sql"""
-                WITH new_person AS (
-                    INSERT INTO people (
-                      record_id
-                    , first_name
-                    , last_name
-                    , other_names
-                    , sex_id
-                    , email_address
-                    , phone_number
-                    , address_line_one
-                    , address_line_two
-                    , zip
-                    ) VALUES (
-                          ${record.id}
-                        , 'fn'
-                        , 'ln'
-                        , 'on'
-                        , 1
-                        , 'test@example.com'
-                        , '0444444444'
-                        , '1 Test St'
-                        , 'Sydney NSW'
-                        , '2000'
-                    ) RETURNING id
-                )
-                INSERT INTO users (person_id, username, password)
-                VALUES (
-                    (SELECT id FROM new_person)
-                  , 'un2'
-                  , 'pw'
-                )
-                """.update.run
-            } else {
-                sql"""
-                WITH existing_person AS (
-                    UPDATE people SET
-                        first_name = 'fn'
-                      , last_name = 'ln'
-                      , other_names = 'on'
-                      , sex_id = 1
-                      , email_address = 'test@example.com'
-                      , phone_number = '0444444444'
-                      , address_line_one = '1 Test St'
-                      , address_line_two = 'Sydney NSW'
-                      , zip = '2000'
-                    WHERE record_id = ${record.id}
-                    RETURNING id
-                )
-                UPDATE users SET
-                    username = 'un'
-                  , password = 'pw'
-                WHERE person_id = (SELECT id FROM existing_person)
-                """.update.run
-            }
-      } yield record.id
+    def upsert(): ConnectionIO[Unit] = {
+        for {
+          record <- sql"""
+              INSERT INTO records (uuid, created_at, created_by)
+              VALUES ('846bc87f-4efe-43cb-9a57-75e20f96db6f', '2020-08-11 08:28:09.517903', 1)
+              ON CONFLICT (uuid)
+              DO UPDATE SET
+                    updated_at = EXCLUDED.created_at
+                  , updated_by = EXCLUDED.created_by
+              RETURNING id, updated_by
+              """.query[RecordReturn].unique
+          val isNew = record.updated_by.isDefined
+          _ <- if (isNew) {
+                  sql"""
+                  WITH new_person AS (
+                      INSERT INTO people (
+                        record_id
+                      , first_name
+                      , last_name
+                      , other_names
+                      , sex_id
+                      , email_address
+                      , phone_number
+                      , address_line_one
+                      , address_line_two
+                      , zip
+                      ) VALUES (
+                            ${record.id}
+                          , 'fn'
+                          , 'ln'
+                          , 'on'
+                          , 1
+                          , 'test@example.com'
+                          , '0444444444'
+                          , '1 Test St'
+                          , 'Sydney NSW'
+                          , '2000'
+                      ) RETURNING id
+                  )
+                  INSERT INTO users (person_id, username, password)
+                  VALUES (
+                      (SELECT id FROM new_person)
+                    , 'un2'
+                    , 'pw'
+                  )
+                  """.update.run
+              } else {
+                  sql"""
+                  WITH existing_person AS (
+                      UPDATE people SET
+                          first_name = 'fn'
+                        , last_name = 'ln'
+                        , other_names = 'on'
+                        , sex_id = 1
+                        , email_address = 'test@example.com'
+                        , phone_number = '0444444444'
+                        , address_line_one = '1 Test St'
+                        , address_line_two = 'Sydney NSW'
+                        , zip = '2000'
+                      WHERE record_id = ${record.id}
+                      RETURNING id
+                  )
+                  UPDATE users SET
+                      username = 'un'
+                    , password = 'pw'
+                  WHERE person_id = (SELECT id FROM existing_person)
+                  """.update.run
+              }
+        } yield ()
+    }
 
     def main(args: Array[String]) {
-        println(program3.transact(xa).unsafeRunSync)
+        println(upsert.transact(xa).unsafeRunSync)
     }
 }
