9f57468c01b4c3180be0efb0e114a0d700803741
Benjamin Glitsos <bengyup@gmail.com>
2020-08-14 21:15:34 +1000

    new db schema now working fully

--- database/init.sh
+++ database/init.sh
@@ -4,95 +4,100 @@ STAFF_DEPARTMENTS=${STAFF_TABLE}_${DEPARTMENTS_TABLE}
 
 psql << EOF
 
+----- Create custom functionality -----
+
+CREATE EXTENSION citext;
+CREATE DOMAIN email AS citext
+    CHECK ( value ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_\`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$' );
+
 ----- Create tables -----
 
 CREATE TABLE $SEX_TABLE (
-    id serial PRIMARY KEY,
-    name VARCHAR(255) UNIQUE NOT NULL
+    id serial PRIMARY KEY
+  , name VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE $RECORDS_TABLE (
-    id SERIAL PRIMARY KEY,
-    uuid UUID UNIQUE NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW(),
-    created_by SMALLINT NOT NULL,
-    edits SMALLINT DEFAULT 0,
-    edited_at TIMESTAMP,
-    edited_by SMALLINT,
-    deleted_at TIMESTAMP,
-    deleted_by SMALLINT
+    id SERIAL PRIMARY KEY
+  , uuid UUID UNIQUE NOT NULL
+  , created_at TIMESTAMP DEFAULT NOW()
+  , created_by SMALLINT NOT NULL
+  , edits SMALLINT DEFAULT 0
+  , edited_at TIMESTAMP
+  , edited_by SMALLINT
+  , deleted_at TIMESTAMP
+  , deleted_by SMALLINT
 );
 
 CREATE TABLE $ROLES_TABLE (
-    id serial PRIMARY KEY,
-    name VARCHAR(255) UNIQUE NOT NULL
+    id serial PRIMARY KEY
+  , name VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE $DEPARTMENTS_TABLE (
-    id serial PRIMARY KEY,
-    name VARCHAR(255) UNIQUE NOT NULL
+    id serial PRIMARY KEY
+  , name VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE $PEOPLE_TABLE (
-    id SERIAL PRIMARY KEY,
-    card_number VARCHAR(12) UNIQUE NOT NULL,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    other_names VARCHAR(255),
-    sex_id SMALLINT NOT NULL,
-    date_of_birth DATE NOT NULL,
-    email_address VARCHAR(255) NOT NULL,
-    phone_number VARCHAR(20) NOT NULL,
-    address_line_one VARCHAR(255) NOT NULL,
-    address_line_two VARCHAR(255),
-    zip VARCHAR(20) NOT NULL,
-    aboriginal_or_torres_strait_islander BOOLEAN NOT NULL,
-    australian_citizen BOOLEAN NOT NULL,
-    born_overseas BOOLEAN NOT NULL,
-    english_second_language BOOLEAN NOT NULL
+    id SERIAL PRIMARY KEY
+  , first_name VARCHAR(255) NOT NULL
+  , last_name VARCHAR(255) NOT NULL
+  , other_names VARCHAR(255)
+  , sex_id SMALLINT NOT NULL
+  , date_of_birth DATE NOT NULL
+  , email_address EMAIL NOT NULL
+  , phone_number VARCHAR(20) NOT NULL
+  , address_line_one VARCHAR(255) NOT NULL
+  , address_line_two VARCHAR(255)
+  , postcode VARCHAR(20) NOT NULL
+  , aboriginal_or_torres_strait_islander BOOLEAN NOT NULL
+  , australian_citizen BOOLEAN NOT NULL
+  , born_overseas BOOLEAN NOT NULL
+  , english_second_language BOOLEAN NOT NULL
 );
 
 CREATE TABLE $STAFF_TABLE (
-    id serial PRIMARY KEY,
-    record_id SMALLINT NOT NULL,
-    person_id SMALLINT NOT NULL,
-    staff_number VARCHAR(12) NOT NULL,
-    employment_start DATE NOT NULL,
-    employment_end DATE
+    id serial PRIMARY KEY
+  , record_id SMALLINT NOT NULL
+  , person_id SMALLINT NOT NULL
+  , staff_number VARCHAR(12) NOT NULL
+  , employment_start DATE NOT NULL
+  , employment_end DATE
 );
 
 CREATE TABLE $PATIENTS_TABLE (
-    id serial PRIMARY KEY,
-    record_id SMALLINT NOT NULL,
-    person_id SMALLINT NOT NULL,
-    patient_number VARCHAR(12) NOT NULL,
-    medicare_number VARCHAR(10),
-    medicare_ref INT,
-    medicare_expiry DATE,
-    UNIQUE(medicare_number, medicare_ref)
+    id serial PRIMARY KEY
+  , record_id SMALLINT NOT NULL
+  , person_id SMALLINT NOT NULL
+  , patient_number VARCHAR(12) NOT NULL
+  , medicare_number VARCHAR(10)
+  , medicare_ref INT
+  , medicare_expiry DATE
+  , UNIQUE(medicare_number, medicare_ref)
 );
 
 CREATE TABLE $STAFF_DEPARTMENTS (
-    staff_id SMALLINT NOT NULL,
-    department_id SMALLINT NOT NULL,
-    PRIMARY KEY(staff_id, department_id)
+    staff_id SMALLINT NOT NULL
+  , department_id SMALLINT NOT NULL
+  , PRIMARY KEY(staff_id, department_id)
 );
 
 CREATE TABLE $VISITS_TABLE (
-    id serial PRIMARY KEY,
-    patient_id SMALLINT NOT NULL,
-    staff_id SMALLINT NOT NULL,
-    start_date DATE NOT NULL,
-    end_date DATE,
-    UNIQUE(patient_id, staff_id, start_date)
+    id serial PRIMARY KEY
+  , patient_id SMALLINT NOT NULL
+  , staff_id SMALLINT NOT NULL
+  , start_date DATE NOT NULL
+  , end_date DATE
+  , UNIQUE(patient_id, staff_id, start_date)
 );
 
 CREATE TABLE $USERS_TABLE (
-    id SERIAL PRIMARY KEY,
-    record_id SMALLINT NOT NULL,
-    person_id SMALLINT UNIQUE NOT NULL,
-    username VARCHAR(20) UNIQUE NOT NULL,
-    password VARCHAR(20) NOT NULL
+    id SERIAL PRIMARY KEY
+  , record_id SMALLINT NOT NULL
+  , staff_id SMALLINT UNIQUE NOT NULL
+  , username VARCHAR(20) UNIQUE NOT NULL
+  , password VARCHAR(20) NOT NULL
 );
 
 ----- Populate with starting values -----
@@ -132,8 +137,7 @@ VALUES
   , ('Vascular');
 
 INSERT INTO $PEOPLE_TABLE (
-    card_number
-  , first_name
+    first_name
   , last_name
   , other_names
   , sex_id
@@ -142,14 +146,13 @@ INSERT INTO $PEOPLE_TABLE (
   , phone_number
   , address_line_one
   , address_line_two
-  , zip
-  , aboriginal_or_torres_strait_islander,
+  , postcode
+  , aboriginal_or_torres_strait_islander
   , australian_citizen
   , born_overseas
   , english_second_language
 ) VALUES (
-    '$SUPER_CARD_NUMBER'
-  , '$SUPER_FIRST_NAME'
+    '$SUPER_FIRST_NAME'
   , '$SUPER_LAST_NAME'
   , '$SUPER_MIDDLE_NAME'
   , '$SUPER_SEX'
@@ -158,7 +161,7 @@ INSERT INTO $PEOPLE_TABLE (
   , '$SUPER_PHONE'
   , '$SUPER_ADDRESS_LINE_1'
   , '$SUPER_ADDRESS_LINE_2'
-  , '$SUPER_ZIP'
+  , '$SUPER_POSTCODE'
   , '$SUPER_ABORIGINAL_OR_TORRES_STRAIT_ISLANDER'
   , '$SUPER_AUSTRALIAN_CITIZEN'
   , '$SUPER_BORN_OVERSEAS'
@@ -195,7 +198,7 @@ INSERT INTO $STAFF_TABLE (
 
 INSERT INTO $USERS_TABLE (
     record_id
-  , person_id
+  , staff_id
   , username
   , password
 ) VALUES (
@@ -268,8 +271,8 @@ FOREIGN KEY (record_id)
 REFERENCES $RECORDS_TABLE (id);
 
 ALTER TABLE $USERS_TABLE
-ADD CONSTRAINT fk_${PEOPLE_TABLE}
-FOREIGN KEY (person_id)
-REFERENCES $PEOPLE_TABLE (id);
+ADD CONSTRAINT fk_${STAFF_TABLE}
+FOREIGN KEY (staff_id)
+REFERENCES $STAFF_TABLE (id);
 
 EOF
