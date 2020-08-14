b4b755f600a47a1307e0fc70aa92cf5e3a4f54f4
Benjamin Glitsos <bengyup@gmail.com>
2020-08-14 19:29:11 +1000

    revised the database schema to make Records identify objects

--- database/init.sh
+++ database/init.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+PEOPLE_ROLES=${PEOPLE_TABLE}_${ROLES_TABLE}
+
 psql << EOF
 
--- Create tables --
+----- Create tables -----
 
 CREATE TABLE $SEX_TABLE (
     id serial PRIMARY KEY,
@@ -12,17 +14,24 @@ CREATE TABLE $SEX_TABLE (
 CREATE TABLE $RECORDS_TABLE (
     id SERIAL PRIMARY KEY,
     uuid UUID UNIQUE NOT NULL,
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
     created_by SMALLINT NOT NULL,
-    updated_at TIMESTAMP,
-    updated_by SMALLINT,
+    edits SMALLINT DEFAULT 0,
+    edited_at TIMESTAMP,
+    edited_by SMALLINT,
     deleted_at TIMESTAMP,
     deleted_by SMALLINT
 );
 
+CREATE TABLE $ROLES_TABLE (
+    id serial PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL
+);
+
 CREATE TABLE $PEOPLE_TABLE (
     id SERIAL PRIMARY KEY,
-    record_id SMALLINT UNIQUE NOT NULL,
+    record_id SMALLINT NOT NULL,
+    card_number VARCHAR(12) UNIQUE NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     other_names VARCHAR(255),
@@ -34,30 +43,33 @@ CREATE TABLE $PEOPLE_TABLE (
     zip VARCHAR(20)
 );
 
+CREATE TABLE $PEOPLE_ROLES (
+    person_id SMALLINT NOT NULL,
+    role_id SMALLINT NOT NULL,
+    PRIMARY KEY(person_id, role_id)
+);
+
 CREATE TABLE $USERS_TABLE (
     id SERIAL PRIMARY KEY,
+    record_id SMALLINT NOT NULL,
     person_id SMALLINT UNIQUE NOT NULL,
     username VARCHAR(20) UNIQUE NOT NULL,
     password VARCHAR(20) NOT NULL
 );
 
--- Populate with starting values --
+----- Populate with starting values -----
 
 INSERT INTO $SEX_TABLE (name) VALUES ('Male'), ('Female');
 
-
-INSERT INTO $USERS_TABLE (
-    person_id
-  , username
-  , password
-) VALUES (
-    1
-  , '$SUPER_USERNAME'
-  , '$SUPER_PASSWORD'
-);
+INSERT INTO $ROLES_TABLE (name)
+VALUES
+    ('Admin')
+  , ('Staff')
+  , ('Patient');
 
 INSERT INTO $PEOPLE_TABLE (
     record_id
+  , card_number
   , first_name
   , last_name
   , other_names
@@ -69,6 +81,7 @@ INSERT INTO $PEOPLE_TABLE (
   , zip
 ) VALUES (
     1
+  , '$SUPER_CARD_NUMBER'
   , '$SUPER_FIRST_NAME'
   , '$SUPER_LAST_NAME'
   , '$SUPER_MIDDLE_NAME'
@@ -80,27 +93,35 @@ INSERT INTO $PEOPLE_TABLE (
   , '$SUPER_ZIP'
 );
 
-INSERT INTO $RECORDS_TABLE (
-    uuid
-  , created_at
-  , created_by
+INSERT INTO $PEOPLE_ROLES (
+    person_id
+  , role_id
 ) VALUES (
-    '$SUPER_UUID'
-  , NOW()
+    1
   , 1
 );
 
--- Create relationships --
+INSERT INTO $RECORDS_TABLE (
+    uuid
+  , created_by
+) VALUES (
+    '$SUPER_UUID'
+  , 1
+);
 
-ALTER TABLE $USERS_TABLE
-ADD CONSTRAINT fk_${PEOPLE_TABLE}
-FOREIGN KEY (person_id)
-REFERENCES $PEOPLE_TABLE (id);
+INSERT INTO $USERS_TABLE (
+    record_id
+  , person_id
+  , username
+  , password
+) VALUES (
+    1
+  , 1
+  , '$SUPER_USERNAME'
+  , '$SUPER_PASSWORD'
+);
 
-ALTER TABLE $PEOPLE_TABLE
-ADD CONSTRAINT fk_${RECORDS_TABLE}
-FOREIGN KEY (record_id)
-REFERENCES $RECORDS_TABLE (id);
+----- Create relationships -----
 
 ALTER TABLE $RECORDS_TABLE
 ADD CONSTRAINT fk_created_by
@@ -108,13 +129,38 @@ FOREIGN KEY (created_by)
 REFERENCES $USERS_TABLE (id);
 
 ALTER TABLE $RECORDS_TABLE
-ADD CONSTRAINT fk_updated_by
-FOREIGN KEY (updated_by)
+ADD CONSTRAINT fk_edited_by
+FOREIGN KEY (edited_by)
 REFERENCES $USERS_TABLE (id);
 
 ALTER TABLE $RECORDS_TABLE
 ADD CONSTRAINT fk_deleted_by
 FOREIGN KEY (deleted_by)
 REFERENCES $USERS_TABLE (id);
+
+ALTER TABLE $PEOPLE_TABLE
+ADD CONSTRAINT fk_${RECORDS_TABLE}
+FOREIGN KEY (record_id)
+REFERENCES $RECORDS_TABLE (id);
+
+ALTER TABLE $PEOPLE_ROLES
+ADD CONSTRAINT fk_person_id
+FOREIGN KEY (person_id)
+REFERENCES $PEOPLE_TABLE (id);
+
+ALTER TABLE $PEOPLE_ROLES
+ADD CONSTRAINT fk_role_id
+FOREIGN KEY (role_id)
+REFERENCES $ROLES_TABLE (id);
+
+ALTER TABLE $USERS_TABLE
+ADD CONSTRAINT fk_${RECORDS_TABLE}
+FOREIGN KEY (record_id)
+REFERENCES $RECORDS_TABLE (id);
+
+ALTER TABLE $USERS_TABLE
+ADD CONSTRAINT fk_${PEOPLE_TABLE}
+FOREIGN KEY (person_id)
+REFERENCES $PEOPLE_TABLE (id);
 
 EOF
