edfa08ad0ba4e86def8d44618d85ec68993b641a
Benjamin Glitsos <bengyup@gmail.com>
2020-08-14 22:26:46 +1000

    init.sh concatenation works locally

--- database/init.sh
+++ database/init.sh
@@ -2,4 +2,10 @@
 
 STAFF_DEPARTMENTS=${STAFF_TABLE}_${DEPARTMENTS_TABLE}
 
-psql "$(cat custom-functionality.sql tables.sql starting-values.sql relationships.sql)"
+eval "cat << EOSQL
+$(cat init/custom-functionality.sql | sed 's#\`#\\`#g' | sed 's#\\#\\\\#g' )
+$(cat init/tables.sql )
+$(cat init/starting-values.sql )
+$(cat init/relationships.sql )
+EOSQL
+" 2> /dev/null
