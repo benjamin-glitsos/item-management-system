278288ae871dbfced52f1bdcdbd5122bc189c8ca
Benjamin Glitsos <bengyup@gmail.com>
2020-08-21 20:36:27 +1000

    Users list filter and sortBy clauses now working

--- controller/src/main/scala/DAO/UsersDAO.scala
+++ controller/src/main/scala/DAO/UsersDAO.scala
@@ -44,12 +44,12 @@ object UsersDAO {
 
     def list() = {
         val q = quote {
-            for {
-              u <- query[User]
-              r <- query[Record]
-                  .join(r => r.id == u.record_id)
-                  .filter(x => x.deleted_at.isEmpty)
-            } yield (u, r)
+            (for {
+                u <- query[User]
+                r <- query[Record].join(r => r.id == u.record_id)
+            } yield (u, r))
+                .filter(x => x._2.deleted_at.isEmpty)
+                .sortBy(x => x._2.edited_at)(Ord.descNullsLast)
         }
         run(q)
     }
