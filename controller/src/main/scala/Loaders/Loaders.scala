86e883dbec97d858c21d51b38e1e016fa0ffe234
Benjamin Glitsos <bengyup@gmail.com>
2020-08-25 21:46:01 +1000

    Users Loader now works. But not finished yet

--- controller/src/main/scala/Loaders/Loaders.scala
+++ controller/src/main/scala/Loaders/Loaders.scala
@@ -1,4 +1,25 @@
+import cats._
+import cats.data._
+import cats.effect._
+import cats.implicits._
+import doobie._
+import doobie.implicits._
+import doobie.util.ExecutionContexts
+import doobie.postgres._
+import doobie.postgres.implicits._
+import org.http4s.server.blaze._
+
 object Loaders {
+    implicit val cs = IO.contextShift(ExecutionContexts.synchronous)
+
+    val xa = Transactor.fromDriverManager[IO](
+        "org.postgresql.Driver",
+        s"jdbc:postgresql://${System.getenv("DATABASE_SERVICE")}/${System.getenv("POSTGRES_DATABASE")}",
+        System.getenv("POSTGRES_USER"),
+        System.getenv("POSTGRES_PASSWORD"),
+        Blocker.liftExecutionContext(ExecutionContexts.synchronous)
+    )
+
     def load(count: Int, nameEnv: String, nameAlt: String, fn: => Unit) = {
         (1 to count) foreach (x => fn)
         println(s"Populated ${sys.env.getOrElse(nameEnv, nameAlt)}")
@@ -9,7 +30,7 @@ object Loaders {
             count = 15,
             nameEnv = "USERS_TABLE",
             nameAlt = "users",
-            fn = UsersLoader.create()
+            fn = UsersLoader.create().transact(xa).unsafeRunSync
         )
     }
 }
--- controller/src/main/scala/Main.scala
+++ controller/src/main/scala/Main.scala
@@ -2,26 +2,9 @@ import cats._
 import cats.data._
 import cats.effect._
 import cats.implicits._
-import java.util.UUID
-import java.time.LocalDateTime
-import doobie._
-import doobie.implicits._
-import doobie.util.ExecutionContexts
-import doobie.postgres._
-import doobie.postgres.implicits._
 import org.http4s.server.blaze._
 
 object Main extends IOApp {
-    implicit val cs = IO.contextShift(ExecutionContexts.synchronous)
-
-    val xa = Transactor.fromDriverManager[IO](
-        "org.postgresql.Driver",
-        s"jdbc:postgresql://${System.getenv("DATABASE_SERVICE")}/${System.getenv("POSTGRES_DATABASE")}",
-        System.getenv("POSTGRES_USER"),
-        System.getenv("POSTGRES_PASSWORD"),
-        Blocker.liftExecutionContext(ExecutionContexts.synchronous)
-    )
-
     def run(args: List[String]): IO[ExitCode] = {
         Loaders.run()
 
