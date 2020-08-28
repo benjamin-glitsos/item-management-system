0bc3bcf3c421d57cc2a5f84fb0da3e2c639f6f67
Benjamin Glitsos <bengyup@gmail.com>
2020-08-28 21:24:44 +1000

    biasedFlip and randomExists tested working. Also LogicUtilities created

--- controller/src/main/scala/Utilities/LogicUtilities.scala
+++ controller/src/main/scala/Utilities/LogicUtilities.scala
@@ -1,9 +1,9 @@
-object LogicUtilities {
+trait LogicUtilities {
     def any(ls: List[Boolean]) = {
-        ls.exists(x)
+        ls.exists(x => x)
     }
 
     def all(ls: List[Boolean]) = {
-        ls.forall(x)
+        ls.forall(x => x)
     }
 }
