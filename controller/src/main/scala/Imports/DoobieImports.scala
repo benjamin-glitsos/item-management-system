4c9bf57ff78ddff6d6a6601837379d07cfdfb6c5
Benjamin Glitsos <bengyup@gmail.com>
2020-08-31 21:16:19 +1000

    Started making Doobie imports package

--- controller/src/main/scala/Imports/DoobieImports.scala
+++ controller/src/main/scala/Imports/DoobieImports.scala
@@ -1,17 +1,19 @@
-trait DoobieImports {
-    import doobie._
-    import doobie.implicits._
-    import doobie.util.ExecutionContexts
-    import doobie.postgres._
-    import doobie.postgres.implicits._
+import doobie._
+import doobie.implicits._
+import doobie.util.ExecutionContexts
+import doobie.postgres._
+import doobie.postgres.implicits._
 
-    implicit val contextShift: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
+package com.bundle.doobie {
+    object imports {
+        implicit val contextShift: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
 
-    val xa = Transactor.fromDriverManager[IO](
-        "org.postgresql.Driver",
-        s"jdbc:postgresql://${System.getenv("DATABASE_SERVICE")}/${System.getenv("POSTGRES_DATABASE")}",
-        System.getenv("POSTGRES_USER"),
-        System.getenv("POSTGRES_PASSWORD"),
-        Blocker.liftExecutionContext(ExecutionContexts.synchronous)
-    )
+        val xa = Transactor.fromDriverManager[IO](
+            "org.postgresql.Driver",
+            s"jdbc:postgresql://${System.getenv("DATABASE_SERVICE")}/${System.getenv("POSTGRES_DATABASE")}",
+            System.getenv("POSTGRES_USER"),
+            System.getenv("POSTGRES_PASSWORD"),
+            Blocker.liftExecutionContext(ExecutionContexts.synchronous)
+        )
+    }
 }
--- controller/src/main/scala/Routes/UsersRoutes.scala
+++ controller/src/main/scala/Routes/UsersRoutes.scala
@@ -7,23 +7,14 @@ import scala.concurrent._
 import cats._
 import cats.data._
 import cats.implicits._
-// import doobie._
-// import doobie.implicits._
-// import doobie.util.ExecutionContexts
-// import doobie.postgres._
-// import doobie.postgres.implicits._
+import doobie._
+import doobie.implicits._
+import doobie.util.ExecutionContexts
+import doobie.postgres._
+import doobie.postgres.implicits._
+import bundle.doobie.imports._
 
 object UsersRoutes {
-    // implicit val contextShift: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
-    //
-    // val xa = Transactor.fromDriverManager[IO](
-    //     "org.postgresql.Driver",
-    //     s"jdbc:postgresql://${System.getenv("DATABASE_SERVICE")}/${System.getenv("POSTGRES_DATABASE")}",
-    //     System.getenv("POSTGRES_USER"),
-    //     System.getenv("POSTGRES_PASSWORD"),
-    //     Blocker.liftExecutionContext(ExecutionContexts.synchronous)
-    // )
-
     object MaybeRestore extends OptionalQueryParamDecoderMatcher[Unit]("restore")
     object MaybeNumber extends OptionalQueryParamDecoderMatcher[Int]("number")
     object MaybeLength extends OptionalQueryParamDecoderMatcher[Int]("length")
