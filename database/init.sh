2d6615ba32acf97cbd49c60f2c86975a8facb140
Benjamin Glitsos <bengyup@gmail.com>
2020-09-07 20:16:37 +1000

    renaming a db init file

--- database/init.sh
+++ database/init.sh
@@ -16,7 +16,7 @@ psql -f /docker-entrypoint-initdb.d/init/domains.sql
 psql << EOP
 $(eval "cat << EOSQL
 $(cat /docker-entrypoint-initdb.d/init/schema.sql)
-$(cat /docker-entrypoint-initdb.d/init/starting-data.sql)
+$(cat /docker-entrypoint-initdb.d/init/data.sql)
 $(cat /docker-entrypoint-initdb.d/init/relationships.sql)
 EOSQL
 " 2> /dev/null)
