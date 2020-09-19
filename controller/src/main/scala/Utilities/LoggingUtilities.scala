53b4c5799dadf8a2d500f508ee06bbebfd5753f2
Benjamin Glitsos <bengyup@gmail.com>
2020-09-19 19:51:58 +1000

    Improved console log and error message headings

--- controller/src/main/scala/Utilities/LoggingUtilities.scala
+++ controller/src/main/scala/Utilities/LoggingUtilities.scala
@@ -1,10 +1,10 @@
 trait LoggingUtilities {
     private def textHeading(size: Int, text: String) = {
-        val decoration = "#" * 7
+        val decoration = "#" * size
         println(s"${decoration} ${message} ${decoration}")
     }
 
     def consoleHeading(text: String) = textHeading(7, text)
 
-    def smallHeading(text: String) = textHeading(3, text)
+    def smallHeading(text: String) = textHeading(5, text)
 }
