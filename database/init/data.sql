4acf808690d8de4d79e0fe5dbb6e97fcea6a0950
Benjamin Glitsos <bengyup@gmail.com>
2020-10-11 11:48:20 +1100

    Removed more SQL relating to the Actions and Objects tables

--- database/init/data.sql
+++ database/init/data.sql
@@ -1,22 +1,3 @@
-INSERT INTO objects (key, name, description, is_in_main_menu)
-VALUES
-    ('users', 'Users', 'The user accounts for this system.', TRUE)
-  , ('stock', 'Stock', 'The stock items that are in this inventory, or have been in the past.', TRUE)
-  , ('transactions', 'Transactions', 'The transactions that have occured for each stock item.', FALSE)
-
-INSERT INTO actions (key, name, description, colour)
-VALUES
-    ('options', 'Options', 'List the available actions.', NULL)
-  , ('list', 'List', 'List the available items in this set.', NULL)
-  , ('open', 'Open', 'Open this item to view or edit it.', NULL)
-  , ('save', 'Save', 'Save any edits you have made to the system.', NULL)
-  , ('create', 'Create New', 'Create a new item.', NULL)
-  , ('delete', 'Delete', 'Delete this item. (This is a soft delete.)', NULL)
-  , ('restore', 'Restore', 'Restore this item which has been deleted.', NULL)
-  , ('hard-delete', 'Hard Delete', 'Permanently delete this item. (This item won’t be recoverable once deleted. This is a hard delete.)', '#be382d')
-  , ('buy', 'Buy', 'Record a transaction in which this stock was bought.', '#318a4a')
-  , ('sell', 'Sell', 'Record a transaction in which this stock was sold.', '#be382d')
-
 WITH user_records_insert AS (
     INSERT INTO records (uuid, created_by)
     VALUES (gen_random_uuid(), 1)
