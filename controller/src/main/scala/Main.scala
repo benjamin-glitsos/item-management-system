38ca7abfc603ef807d43a31f276d779fac4f435a
Benjamin Glitsos <bengyup@gmail.com>
2020-10-12 09:47:45 +1100

    Added composed middleware but untested

--- controller/src/main/scala/Main.scala
+++ controller/src/main/scala/Main.scala
@@ -13,7 +13,7 @@ object Main extends IOApp with TextUtilities with EnvUtilities {
                     System.getenv("CONTROLLER_PORT").toInt,
                     System.getenv("DOCKER_LOCALHOST")
                 )
-                .withHttpApp(ReqContractValidationMiddle(Routes.router)) // TODO: compose middleware into one, like this: https://github.com/http4s/http4s/issues/1967
+                .withHttpApp(Middleware.middleware(Routes.router)) // ReqContractValidationMiddle(Routes.router)
                 .serve
                 .compile
                 .drain
