dec57157008ee2690be09ca3e7e48e78d0e7d741
Benjamin Glitsos <bengyup@gmail.com>
2020-10-16 00:34:02 +1100

    created jsonGet utility to simplify routes

--- controller/src/main/scala/Utilities/JsonUtilities.scala
+++ controller/src/main/scala/Utilities/JsonUtilities.scala
@@ -1,7 +1,8 @@
 import io.circe.Json
+import monocle.{Optional => MonocleOption}
 
 trait JsonUtilities {
-    def jsonGet[A](lens: monocle.Optional[Json, A], json: Json): A = {
+    def jsonGet[A](lens: MonocleOption[Json, A], json: Json): A = {
         lens.getOption(json).get
     }
 }
