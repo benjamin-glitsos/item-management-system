db31f138bebc4c377d41faebaab75ee89d225148
Benjamin Glitsos <bengyup@gmail.com>
2020-10-25 21:39:44 +1100

    Moved almost all TODOs to TickTick. That is the new board now. Also moved away from Jira

--- controller/src/main/scala/Routes/UsersRoutes.scala
+++ controller/src/main/scala/Routes/UsersRoutes.scala
@@ -26,50 +26,6 @@ import org.json.JSONObject
 import org.json.JSONTokener
 
 object UsersRoutes extends ValidationUtilities {
-    // TODO: USE UJSON
-    // TODO: USE SCALATEST
-    // TODO: USE AKKA HTTP
-    // TODO: delete endpoint will take a "method" of soft, hard or restore.
-    // TODO: delete endpoint will take a list of UUIDs. The users delete service will actually be a passthrough that calls the meta delete service which actually contains the functionality.
-    // TODO: create psql trigger to encrypt password using env file password as key. Create new file: triggers.sql or functions.sql?
-    // TODO: use kompose for converting docker compose to kubernetes
-    // TODO: use JSON Schema within Controller and later create an API that sends it to the Portal to be converted into OpenAPI format and combined with the OpenAPI YAML then used in an Angular component of the Swagger UI.
-    // TODO: try using built-in auth middleware. Middleware will be: https, auto-slash, auth. But later you will probably make your own auth middleware so that you can add the user roles to the JWT and then also check access control within the same middleware.
-    // TODO: try circe/circe-json-schema returning JSON with defaults within Valid() otherwise use everit-org/json-schema. But circe-json-schema is so small you can just copy most of the code
-    // TODO: Controller folder structure:
-    // src/main/scala/
-    // src/test/scala/
-    // TODO: use apply method for objects like Routers and Seeders so that they can be used like Routers()
-    // TODO: use kompose for converting docker compose to kubernetes
-    // TODO: the XML will contain default values. Then it will return the XML tree-based data. Then you just directly get values from that tree. And you can use optional accessors for optional values and these will return Some/None. Therefore, all of this will now be handled by XSD.
-    // TODO: instead of 'field' in Error case class, use mandatory 'id' String (don't use Option). Then use same id attribute on form fields on front-end. And these id attributes are determined in the XSD (by using id attribute)
-    // TODO: Use database Views plus JSON to merge commonly-merged tables into one and then just use the Quill/Doobie to work with these Views. Multiple columns are merged into one JSON column. Have a new views.sql file for this. These Views will be:
-    // * users_with_roles
-    // * transactions_with_type
-    // * records_with_users (json fields: username, avatar)
-    // TODO: casbin error message will always be the same: "access_denied", "You do not have permission to '$action' this '$object' resource at this time."
-    // TODO: casbin model will be ABAC with roles and superuser role
-    // TODO: add additional information to the json schemas and then use that to generate the front-end forms automatically? Have an api parameter that requests the schema of an endpoint and it will return properties like description, bootstrap-column
-    // TODO: make logging middleware that prints to console for now. A good simple middleware to create firstly
-    // TODO: hash passwords asyncronously (use ZIO)
-    // Overall routing structure is:
-    // / => Front-end
-    // /api/ => API
-    // /dev/ => dev portal (serve a plain HTML page which lists the dev pages. you will create a 'views' folder)
-    // /dev/adminer/
-    // /dev/api-docs/
-    // /dev/db-docs/
-    // TODO: create routing files:
-    // RootRoutes.scala
-    // DevRoutes.scala
-    // ApiRoutes.scala
-    // TODO: make two functions for casbin: one to validate a request, and the other to validate a list of actions to return only the ones that can be accessed. This will map casbin over the list of actions but keep the other parameters the same between each step in the map
-    // TODO: add a cache control middleware to disable all caching
-    // TODO: add server redirect middleware http to https
-    // TODO: add error formatting middleware that groups by id. But have an optional parameter to turn this off.
-    // TODO: make a middleware folder
-    // TODO: remove NGINX from angular container
-
     val endpoints = HttpRoutes.of[IO] {
         case req @ POST -> Root / "list" => {
             // try (InputStream inputStream = getClass().getResourceAsStream("/path/to/your/schema.json")) {
@@ -92,8 +48,6 @@ object UsersRoutes extends ValidationUtilities {
         case _ => NotFound(Errors.resourceNotFound()).unsafeRunSync()
 
        // case GET -> Root / username => {
-       //      // TODO: add user_username to the body
-       //
        //      Ok(UsersServices.open(username, System.getenv("SUPER_ADMIN_USERNAME")).transact(xa).unsafeRunSync)
        //  }
 
@@ -146,15 +100,6 @@ object UsersRoutes extends ValidationUtilities {
         // }
 
         // case PATCH -> Root => {
-        //     // TODO: the body will be like:
-        //     // {
-        //     //     username: String,
-        //     //     data: {
-        //     //         user: User,
-        //     //         notes: String
-        //     //     },
-        //     //     user_username: String
-        //     // }
         //     Ok(UsersServices.edit(
         //         User(
         //             id = 2,
@@ -170,7 +115,7 @@ object UsersRoutes extends ValidationUtilities {
 
         // case body @ DELETE -> Root => {
         //     for {
-        //         json <- body.as[Json] // TODO: xml <- body map { validateXML(parseXML(_)) }
+        //         json <- body.as[Json]
         //
         //         val action = Validators.getRequiredField("action", json).andThen { action_ =>
         //             Validators.isDeleteActionSupported(x.action)
