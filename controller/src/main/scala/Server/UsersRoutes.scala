a800d91453e18fd86b0f900ee1aca3177c05597c
Benjamin Glitsos <bengyup@gmail.com>
2020-12-20 23:04:44 +1100

    Started JSON Schema validation. Getting the json resource works but the everit code isnt working yet.

--- controller/src/main/scala/Server/UsersRoutes.scala
+++ controller/src/main/scala/Server/UsersRoutes.scala
@@ -2,13 +2,11 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.model._
 
-// TODO: make this route/UsersService use the request JSON and validate it using the everit JSON Schema package.
 // TODO: make custom error response which is the same but using JSON. E.g. { errors: ["The requested object was not ..."] }
 // TODO: make sure this is async and nothing is blocking
 
 object UsersRoutes {
   def apply(entity: String): Route =
-    // TODO: first send entity (keeping it as a string) to the json validator. Then parse it using uPickle so that you can get the values out of it and pass them to UsersService
     concat(
         get(
             complete(
--- controller/src/main/scala/Services/UsersServices.scala
+++ controller/src/main/scala/Services/UsersServices.scala
@@ -3,6 +3,10 @@ import java.time.format.DateTimeFormatter
 import upickle.default._
 import doobie.implicits._
 import bundles.doobie.connection._
+import org.everit.json.schema.Schema
+import org.everit.json.schema.loader.SchemaLoader
+import org.json.JSONObject
+import scala.io.Source
 
 object UsersServices {
   implicit val localDateTimeReadWrite: ReadWriter[LocalDateTime] =
@@ -18,6 +22,20 @@ object UsersServices {
   implicit val rw: ReadWriter[UsersList] = macroRW
 
   def list(entityBody: String): String = {
+    val schemaSource = Source.fromResource("schemas/users-list.json")
+    val schemaString =
+      try schemaSource.mkString
+      finally schemaSource.close()
+    val rawSchema: JSONObject = new JSONObject(schemaString)
+    val schema: Schema        = SchemaLoader.load(rawSchema)
+    println(
+        schema.validate(
+            new JSONObject(entityBody)
+        )
+    )
+
+    // TODO: pass the succesful validation (with default values) to ujson, reject unsuccessful with your standardised error format
+
     val body: ujson.Value = ujson.read(entityBody)
     val offset            = body("offset").num.toInt
     val pageLength        = body("page_length").num.toInt
