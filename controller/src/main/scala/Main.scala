c3a3e252bce49727c88f95763c634ab658d29603
Benjamin Glitsos <bengyup@gmail.com>
2020-12-20 11:19:01 +1100

    Akka routing DSL working - simple test

--- controller/src/main/scala/Main.scala
+++ controller/src/main/scala/Main.scala
@@ -14,17 +14,16 @@ object Main {
     implicit val system           = ActorSystem(Behaviors.empty, "my-system")
     implicit val executionContext = system.executionContext
 
-    val route =
-      path("hello") {
-        get {
-          complete(
-              HttpEntity(
-                  ContentTypes.`text/html(UTF-8)`,
-                  "<h1>Say hello to akka-http</h1>"
-              )
-          )
-        }
-      }
+    val route = get {
+      concat(
+          path("hello") {
+            complete("Wow")
+          },
+          path("world") {
+            complete("Cool")
+          }
+      )
+    }
 
     val bindingFuture =
       Http()
@@ -40,12 +39,12 @@ object Main {
     do {
       if (command_line != "" && command_line != "exit") {
         println(
-            s"${Console.RED} The command '$command_line' does not exist. ${Console.WHITE}"
+            Console.RED + s"The command '$command_line' does not exist." + Console.WHITE
         )
       }
       command_line = StdIn
         .readLine(
-            s"${Console.GREEN} Use 'exit' to shutdown the server... ${Console.WHITE}"
+            Console.GREEN + "Use 'exit' to shutdown the server... " + Console.WHITE
         )
         .stripMargin
         .stripLineEnd
