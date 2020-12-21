9d72d4de4c52412711a025c617cd875e1fc910fe
Benjamin Glitsos <bengyup@gmail.com>
2020-12-21 13:41:15 +1100

    Now attempting to use Try Success Failure but it isnt working yet

--- controller/src/main/scala/Directives/SchemaValidate.scala
+++ controller/src/main/scala/Directives/SchemaValidate.scala
@@ -6,6 +6,7 @@ import scala.io.Source
 import scala.jdk.CollectionConverters._
 import akka.http.scaladsl.server.Directives._
 import scala.concurrent.duration._
+import scala.util.{Try, Success, Failure}
 
 object SchemaValidate {
   def apply(schemaFilename: String): Directive1[String] =
@@ -34,21 +35,21 @@ object SchemaValidate {
           .load()
           .build()
 
-      schema.validate(entityObject) match {
-        case e: ValidationException => {
-          val validationIntro: String = e.getMessage()
-
-          val validationErrors: Seq[String] =
-            e.getCausingExceptions().asScala.map(_.getMessage()).toSeq
-
-          val validationAll: Seq[String] =
-            validationIntro +: validationErrors
-
-          reject(ValidationRejection(validationAll.mkString("\n")))
+      Try(schema.validate(entityObject)) match {
+        case Success(_) => pass
+        case Failure(e) => {
+          // val validationIntro: String = e.getMessage()
+          //
+          // val validationErrors: Seq[String] =
+          //   e.getCausingExceptions().asScala.map(_.getMessage()).toSeq
+          //
+          // val validationAll: Seq[String] =
+          //   validationIntro +: validationErrors
+          //
+          // reject(ValidationRejection(validationAll.mkString("\n")))
+          println(e.getMessage())
+          e
         }
-        case e: Throwable =>
-          reject(ValidationRejection(e.toString()))
-        case success: Unit => pass
       }
 
       entityObject.toString()
