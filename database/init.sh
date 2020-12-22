96c57f2f1286b11bc363f7dd359709f741d5ed6c
Benjamin Glitsos <bengyup@gmail.com>
2020-12-22 23:02:37 +1100

    init.sh refactored a bit more

--- database/init.sh
+++ database/init.sh
@@ -4,18 +4,17 @@ PSQL_SCRIPTS="/docker-entrypoint-initdb.d/init"
 
 psql << PSQL
 $(cat $PSQL_SCRIPTS/domains.sql)
+
 $(cat $PSQL_SCRIPTS/tables.sql)
-PSQL
 
-psql << INTERPOLATED_PSQL
-$(eval "cat << PSQL
+$(eval "cat << PRE_INTERPOLATED_PSQL
 $(cat $PSQL_SCRIPTS/data.sql)
-PSQL
+PRE_INTERPOLATED_PSQL
 " 2> /dev/null)
-INTERPOLATED_PSQL
 
-psql << PSQL
 $(cat $PSQL_SCRIPTS/relationships.sql)
+
 $(cat $PSQL_SCRIPTS/views.sql)
+
 $(cat $PSQL_SCRIPTS/triggers.sql)
 PSQL
