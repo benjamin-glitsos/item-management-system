aef4cb59573e55c7ce60f292bf7a3ad1e3a96950
Benjamin Glitsos <bengyup@gmail.com>
2020-12-22 17:48:04 +1100

    Added another todo/note

--- database/init/triggers.sql
+++ database/init/triggers.sql
@@ -16,6 +16,7 @@ CREATE TRIGGER users_open_modifiable_view_trigger
     FOR EACH ROW EXECUTE PROCEDURE users_open_modifiable_view();
 
 -- NOTE: that the users_open view joins in the meta_opens view. It is a view joining another view. And the meta_opens view merges in numerous tables. The trigger will need to support that.
+-- TODO: simplify the meta table to: created at, updated at, deleted at, restored at, is_deleted then delete the two meta views?
 
 -- TODO: test
 -- update users_open set opens = 1 where username = 'bengyup';
