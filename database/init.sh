730662a67bf27d58079e9008a00d0759b0246812
Benjamin Glitsos <bengyup@gmail.com>
2020-12-22 17:34:52 +1100

    Tidied up the init.sh bash script

--- database/init.sh
+++ database/init.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-psql -f /docker-entrypoint-initdb.d/init/domains.sql
-psql -f /docker-entrypoint-initdb.d/init/tables.sql
-psql << EOP
-$(eval "cat << EOSQL
-$(cat /docker-entrypoint-initdb.d/init/data.sql)
-EOSQL
-" 2> /dev/null)
-EOP
-psql -f /docker-entrypoint-initdb.d/init/relationships.sql
-psql -f /docker-entrypoint-initdb.d/init/views.sql
-psql -f /docker-entrypoint-initdb.d/init/triggers.sql
+PSQL_SCRIPTS="/docker-entrypoint-initdb.d/init"
 
-# TODO: tidy this code. Reduce redundancy.
+psql << PSQL
+$(cat $PSQL_SCRIPTS/domains.sql)
+$(cat $PSQL_SCRIPTS/tables.sql)
+PSQL
+
+psql << INTERPOLATED_PSQL
+$(eval "cat << PSQL
+$(cat $PSQL_SCRIPTS/data.sql)
+PSQL
+" 2> /dev/null)
+INTERPOLATED_PSQL
+
+psql << PSQL
+$(cat $PSQL_SCRIPTS/relationships.sql)
+$(cat $PSQL_SCRIPTS/views.sql)
+$(cat $PSQL_SCRIPTS/triggers.sql)
+PSQL
