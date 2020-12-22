557d1567d7037d6074d219a019c70156af9262df
Benjamin Glitsos <bengyup@gmail.com>
2020-12-23 10:04:48 +1100

    Some todos

--- controller/src/main/scala/Server/UsersRoutes.scala
+++ controller/src/main/scala/Server/UsersRoutes.scala
@@ -2,6 +2,8 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.model._
 
+// TODO: write the test cases after you set up your deployment pipeline so you make sure you can integrate the testing into that
+// TODO: remove the redundancy in the complete HttpEntity directives. Merge them into a single CompleteJson directive?
 // TODO: move the properties from delete-users.json to definitions/deletion.json. Name them 'method' and 'keysOfItems'
 // TODO: optionally, make all of the error handlers use json by using a directive on the ApiRoutes: { errors: [{ code: "example_error", message: "Example error message." }] }
 
