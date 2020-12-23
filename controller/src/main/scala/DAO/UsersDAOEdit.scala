98e875ccf5211822fd2d07913451b030a7509c89
Benjamin Glitsos <bengyup@gmail.com>
2020-12-24 10:10:06 +1100

    Started troubleshooting the doobie sql query until I can make it work

--- controller/src/main/scala/DAO/UsersDAOEdit.scala
+++ controller/src/main/scala/DAO/UsersDAOEdit.scala
@@ -1,6 +1,13 @@
 import doobie_bundle.database.dc._
-import doobie.Fragments
+import doobie.Fragment
 import doobie.Fragments.{setOpt}
+import doobie._
+import doobie.implicits._
+import doobie.util.ExecutionContexts
+import cats._
+import cats.data._
+import cats.effect._
+import cats.implicits._
 
 // TODO: https://javadoc.io/doc/org.tpolecat/doobie-core_2.12/latest/doobie/util/fragments$.html
 
@@ -12,15 +19,8 @@ trait UsersDAOEdit {
       emailAddress: Option[String],
       notes: Option[String]
   ) = {
-    val usernameFr: Fragment = newUsername.map(s => fr"username == $s")
-    sql"UPDATE users_with_meta ${setOpt(usernameFr)} WHERE username = '$oldUsername'".update
+    // val usernameFr: Fragment = newUsername.map(s => fr"username == $s")
+    // sql"UPDATE users_with_meta ${setOpt(usernameFr)} WHERE username = '$oldUsername'".update
+    sql"UPDATE users_with_meta SET password = 'edited-password' WHERE username = 'demo_admin'".update
   }
 }
-// _.username,
-// username,
-// _.password,
-// password,
-// _.email_address,
-// email_address,
-// _.notes,
-// notes
