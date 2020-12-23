92cb1c290b15f242c3e593ec3791cc9f87d3279d
Benjamin Glitsos <bengyup@gmail.com>
2020-12-24 01:44:13 +1100

    A todo

--- controller/src/main/scala/Routes/UsersRoutes.scala
+++ controller/src/main/scala/Routes/UsersRoutes.scala
@@ -3,7 +3,8 @@ import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.model._
 
 // TODO: write the test cases after you set up your deployment pipeline so you make sure you can integrate the testing into that
-// TODO: put the fields in create-user.json into a definitions file?
+// TODO: just extract only the ServerShutdown part into  anew file. Then delete the comment headings. You will need to accept a function argument of the bindingFuture (pass in an anonymous function) then execute it. E.g. apply(bindingFutureFun: () => Future[ServerBinding]) = { bindingFutureFun() }
+// TODO: put the fields in create-user.json into definitions/fields.json ?
 // TODO: remove the redundancy in the complete HttpEntity directives. Merge them into a single CompleteJson directive?
 // TODO: optionally, make all of the error handlers use json by using a directive on the ApiRoutes: { errors: [{ code: "example_error", message: "Example error message." }] }
 
