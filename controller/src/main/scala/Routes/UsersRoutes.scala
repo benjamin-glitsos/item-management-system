43b1e419b48f3378f50c88b520fc4512ea73afdb
Benjamin Glitsos <bengyup@gmail.com>
2020-12-24 00:52:40 +1100

    Moved Server object to its own layer and renamed the Routes layer

--- controller/src/main/scala/Routes/UsersRoutes.scala
+++ controller/src/main/scala/Routes/UsersRoutes.scala
@@ -3,7 +3,6 @@ import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.model._
 
 // TODO: write the test cases after you set up your deployment pipeline so you make sure you can integrate the testing into that
-// TODO: move Server to new layer than Routes and split it into multiple object (that have 'apply' methods) then call those objects
 // TODO: remove the redundancy in the complete HttpEntity directives. Merge them into a single CompleteJson directive?
 // TODO: optionally, make all of the error handlers use json by using a directive on the ApiRoutes: { errors: [{ code: "example_error", message: "Example error message." }] }
 
--- controller/src/main/scala/Server/Server.scala
+++ controller/src/main/scala/Server/Server.scala
@@ -5,54 +5,56 @@ import scala.io.StdIn
 import org.fusesource.jansi.AnsiConsole
 import org.fusesource.jansi.Ansi._
 import org.fusesource.jansi.Ansi.Color._
+import scala.concurrent.Future
+import akka.http.scaladsl.Http.ServerBinding
 
 object Server {
   def apply(): Unit = {
-    implicit val system           = ActorSystem(Behaviors.empty, "my-system")
+    implicit val system           = ActorSystem(Behaviors.empty, "actor-system")
     implicit val executionContext = system.executionContext
 
     AnsiConsole.systemInstall();
 
+    val host: String  = System.getenv("DOCKER_LOCALHOST")
+    val port: Integer = System.getenv("CONTROLLER_PORT").toInt
+
     // Server start --------------------
 
-    val bindingFuture =
+    val bindingFuture: Future[ServerBinding] =
       Http()
-        .newServerAt(
-            System.getenv("DOCKER_LOCALHOST"),
-            System.getenv("CONTROLLER_PORT").toInt
-        )
+        .newServerAt(host, port)
         .bind(Routes())
 
     println(
-        ansi()
-          .a("\n")
-          .fg(BLUE)
-          .a("The server is online at ")
-          .bold()
-          .a("http://localhost/")
-          .reset()
-          .a("\n")
+      ansi()
+        .a("\n")
+        .fg(BLUE)
+        .a("The server is online at ")
+        .bold()
+        .a(s"http://localhost/")
+        .reset()
+        .a("\n")
     )
 
     // Graceful shutdown --------------------
 
-    var terminal_input = ""
+    var terminal_input: String = ""
     do {
       if (terminal_input != "" && terminal_input != "exit") {
         println(
-            ansi()
-              .fg(RED)
-              .a(s"The command '$terminal_input' does not exist")
-              .reset()
+          ansi()
+            .fg(RED)
+            .a(s"The command '$terminal_input' does not exist")
+            .reset()
         );
       }
       terminal_input = StdIn
         .readLine(
-            ansi()
-              .fg(GREEN)
-              .a("Use 'exit' to shutdown the server... \n")
-              .reset()
-              .toString()
+          ansi()
+            .fg(GREEN)
+            .a("Use 'exit' to shutdown the server... \n")
+            .reset()
+            .toString()
         )
         .stripMargin
         .stripLineEnd
