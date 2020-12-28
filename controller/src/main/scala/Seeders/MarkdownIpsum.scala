9e6c431d2ce49122f731861ab6df1dad7752599d
Benjamin Glitsos <bengyup@gmail.com>
2020-12-29 08:37:19 +1100

    Markdown generator now working properly, just some finishing touches to add

--- controller/src/main/scala/Seeders/MarkdownIpsum.scala
+++ controller/src/main/scala/Seeders/MarkdownIpsum.scala
@@ -3,20 +3,16 @@ import com.devskiller.jfairy.producer.text.TextProducer
 
 object MarkdownIpsum extends SeederTrait {
   private final def hasContent()         = biasedCoinFlip(probability = 0.75)
-  private final def numberOfParagraphs() = Random.between(1, 2)
-  private final def numberOfSentences()  = Random.between(1, 3)
-  private final def hasHeading()         = biasedCoinFlip(0.75)
+  private final def numberOfParagraphs() = Random.between(1, 3)
+  private final def numberOfSentences()  = Random.between(1, 5)
+  private final def hasHeading()         = biasedCoinFlip(0.5)
 
   final def apply(textProducer: TextProducer): String = {
     if (hasContent()) {
       (1 to numberOfParagraphs)
         .map(paragraph => {
-          val heading: Option[String] =
-            if (hasHeading()) {
-              Some(s"## ${textProducer.latinSentence(1)}")
-            } else {
-              None
-            }
+          val heading: Option[String] = Option
+            .when(hasHeading()) { s"## ${textProducer.latinSentence(1)}" }
           val content: String = textProducer.latinSentence(numberOfSentences())
           heading ++ List(content)
         })
