71dcf4acccbdee52b538dac3a5de8da617d3a3b1
Benjamin Glitsos <bengyup@gmail.com>
2020-12-31 22:29:37 +1100

    Started adding ag-grid, but not working yet

--- admin-angular/src/app/app.component.ts
+++ admin-angular/src/app/app.component.ts
@@ -3,21 +3,34 @@
  * Copyright Akveo. All Rights Reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
-import { Component, OnInit } from '@angular/core';
-import { AnalyticsService } from './@core/utils/analytics.service';
-import { SeoService } from './@core/utils/seo.service';
+import { Component, OnInit } from "@angular/core";
+import { AnalyticsService } from "./@core/utils/analytics.service";
+import { SeoService } from "./@core/utils/seo.service";
 
 @Component({
-  selector: 'ngx-app',
-  template: '<router-outlet></router-outlet>',
+    selector: "ngx-app",
+    template: "<router-outlet></router-outlet>"
 })
 export class AppComponent implements OnInit {
+    constructor(
+        private analytics: AnalyticsService,
+        private seoService: SeoService
+    ) {}
 
-  constructor(private analytics: AnalyticsService, private seoService: SeoService) {
-  }
+    ngOnInit(): void {
+        this.analytics.trackPageViews();
+        this.seoService.trackCanonicalChanges();
+    }
 
-  ngOnInit(): void {
-    this.analytics.trackPageViews();
-    this.seoService.trackCanonicalChanges();
-  }
+    columnDefs = [
+        { headerName: "Make", field: "make" },
+        { headerName: "Model", field: "model" },
+        { headerName: "Price", field: "price" }
+    ];
+
+    rowData = [
+        { make: "Toyota", model: "Celica", price: 35000 },
+        { make: "Ford", model: "Mondeo", price: 32000 },
+        { make: "Porsche", model: "Boxter", price: 72000 }
+    ];
 }
