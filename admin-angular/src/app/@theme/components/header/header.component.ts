55f07d514ed71d4fac288ccfc24a115f1bfb0240
Benjamin Glitsos <bengyup@gmail.com>
2020-12-31 21:33:37 +1100

    Removed some unneeded theming and content

--- admin-angular/src/app/@theme/components/header/header.component.ts
+++ admin-angular/src/app/@theme/components/header/header.component.ts
@@ -1,94 +1,85 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { NbMediaBreakpointsService, NbMenuService, NbSidebarService, NbThemeService } from '@nebular/theme';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import {
+    NbMediaBreakpointsService,
+    NbMenuService,
+    NbSidebarService,
+    NbThemeService
+} from "@nebular/theme";
 
-import { UserData } from '../../../@core/data/users';
-import { LayoutService } from '../../../@core/utils';
-import { map, takeUntil } from 'rxjs/operators';
-import { Subject } from 'rxjs';
+import { UserData } from "../../../@core/data/users";
+import { LayoutService } from "../../../@core/utils";
+import { map, takeUntil } from "rxjs/operators";
+import { Subject } from "rxjs";
 
 @Component({
-  selector: 'ngx-header',
-  styleUrls: ['./header.component.scss'],
-  templateUrl: './header.component.html',
+    selector: "ngx-header",
+    styleUrls: ["./header.component.scss"],
+    templateUrl: "./header.component.html"
 })
 export class HeaderComponent implements OnInit, OnDestroy {
+    private destroy$: Subject<void> = new Subject<void>();
+    userPictureOnly: boolean = false;
+    user: any;
 
-  private destroy$: Subject<void> = new Subject<void>();
-  userPictureOnly: boolean = false;
-  user: any;
+    currentTheme = "corporate";
 
-  themes = [
-    {
-      value: 'default',
-      name: 'Light',
-    },
-    {
-      value: 'dark',
-      name: 'Dark',
-    },
-    {
-      value: 'cosmic',
-      name: 'Cosmic',
-    },
-    {
-      value: 'corporate',
-      name: 'Corporate',
-    },
-  ];
+    userMenu = [{ title: "Profile" }, { title: "Log out" }];
 
-  currentTheme = 'default';
+    constructor(
+        private sidebarService: NbSidebarService,
+        private menuService: NbMenuService,
+        private themeService: NbThemeService,
+        private userService: UserData,
+        private layoutService: LayoutService,
+        private breakpointService: NbMediaBreakpointsService
+    ) {}
 
-  userMenu = [ { title: 'Profile' }, { title: 'Log out' } ];
+    ngOnInit() {
+        this.currentTheme = this.themeService.currentTheme;
 
-  constructor(private sidebarService: NbSidebarService,
-              private menuService: NbMenuService,
-              private themeService: NbThemeService,
-              private userService: UserData,
-              private layoutService: LayoutService,
-              private breakpointService: NbMediaBreakpointsService) {
-  }
+        this.userService
+            .getUsers()
+            .pipe(takeUntil(this.destroy$))
+            .subscribe((users: any) => (this.user = users.nick));
 
-  ngOnInit() {
-    this.currentTheme = this.themeService.currentTheme;
+        const { xl } = this.breakpointService.getBreakpointsMap();
+        this.themeService
+            .onMediaQueryChange()
+            .pipe(
+                map(([, currentBreakpoint]) => currentBreakpoint.width < xl),
+                takeUntil(this.destroy$)
+            )
+            .subscribe(
+                (isLessThanXl: boolean) => (this.userPictureOnly = isLessThanXl)
+            );
 
-    this.userService.getUsers()
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((users: any) => this.user = users.nick);
+        this.themeService
+            .onThemeChange()
+            .pipe(
+                map(({ name }) => name),
+                takeUntil(this.destroy$)
+            )
+            .subscribe(themeName => (this.currentTheme = themeName));
+    }
 
-    const { xl } = this.breakpointService.getBreakpointsMap();
-    this.themeService.onMediaQueryChange()
-      .pipe(
-        map(([, currentBreakpoint]) => currentBreakpoint.width < xl),
-        takeUntil(this.destroy$),
-      )
-      .subscribe((isLessThanXl: boolean) => this.userPictureOnly = isLessThanXl);
+    ngOnDestroy() {
+        this.destroy$.next();
+        this.destroy$.complete();
+    }
 
-    this.themeService.onThemeChange()
-      .pipe(
-        map(({ name }) => name),
-        takeUntil(this.destroy$),
-      )
-      .subscribe(themeName => this.currentTheme = themeName);
-  }
+    changeTheme(themeName: string) {
+        this.themeService.changeTheme(themeName);
+    }
 
-  ngOnDestroy() {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
+    toggleSidebar(): boolean {
+        this.sidebarService.toggle(true, "menu-sidebar");
+        this.layoutService.changeLayoutSize();
 
-  changeTheme(themeName: string) {
-    this.themeService.changeTheme(themeName);
-  }
+        return false;
+    }
 
-  toggleSidebar(): boolean {
-    this.sidebarService.toggle(true, 'menu-sidebar');
-    this.layoutService.changeLayoutSize();
-
-    return false;
-  }
-
-  navigateHome() {
-    this.menuService.navigateHome();
-    return false;
-  }
+    navigateHome() {
+        this.menuService.navigateHome();
+        return false;
+    }
 }
