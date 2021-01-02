a86dd3a715e7e4f5b83c609b8633e912a4872597
Benjamin Glitsos <bengyup@gmail.com>
2021-01-02 21:44:09 +1100

    Working on the ag-grid

--- admin-angular/src/app/pages/users/list/list.component.ts
+++ admin-angular/src/app/pages/users/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
     selector: "ngx-users-list",
@@ -6,21 +7,37 @@ import { Component } from "@angular/core";
     styleUrls: ["./list.component.scss"]
 })
 export class UsersListComponent {
-    defaultColDef = { type: "nonEditableColumn" };
+    defaultColDef = { editable: false, flex: 1, minWidth: 100 };
 
     columnDefs = [
         { headerName: "Username", field: "username" },
         { headerName: "Email Address", field: "email_address" },
-        { headerName: "Edited At", field: "edited_at" },
-        { headerName: "Actions", field: "actions" }
+        {
+            headerName: "Created At",
+            field: "created_at",
+            filter: "agDateColumnFilter"
+        },
+        {
+            headerName: "Edited At",
+            field: "edited_at",
+            filter: "agDateColumnFilter"
+        },
+        { headerName: "Actions", field: "actions", maxWidth: 200 }
     ];
 
     rowData = [
         {
             username: "Toyota",
             email_address: "Celica",
+            created_at: 34000,
             edited_at: 35000,
             actions: "..."
         }
     ];
+
+    // onGridReady(params) {
+    //     this.http.get("http://localhost/api/v1/users/").subscribe(data => {
+    //         this.rowData = data.data;
+    //     });
+    // }
 }
