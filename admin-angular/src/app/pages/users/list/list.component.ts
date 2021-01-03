469c3cba24e6de42fd94841784edd6f47b4ec2dc
Benjamin Glitsos <bengyup@gmail.com>
2021-01-03 20:22:35 +1100

    Menu now has proper values and placeholders

--- admin-angular/src/app/pages/users/list/list.component.ts
+++ admin-angular/src/app/pages/users/list/list.component.ts
@@ -100,132 +100,30 @@ export class UsersListComponent {
         }
     ];
 
-    actions: Array<{ id: Number; text: String; icon: String }> = [
-        { id: 1, text: "My profile", icon: "user" },
-        { id: 2, text: "Messages", icon: "email" },
-        { id: 3, text: "Contacts", icon: "group" },
-        { id: 4, text: "Log out", icon: "runner" }
-    ];
-
-    logAction(e) {
-        console.log(e.itemData.text + " was clicked");
-    }
-
-    logButtonClick() {
-        console.log("Main button was clicked");
-    }
-
     products: Product[] = [
         {
-            id: "1",
-            name: "Video Players",
-            items: [
-                {
-                    id: "1_1",
-                    name: "HD Video Player",
-                    price: 220,
-                    icon: "images/products/1.png"
-                },
-                {
-                    id: "1_2",
-                    name: "SuperHD Video Player",
-                    icon: "images/products/2.png",
-                    price: 270
-                }
-            ]
+            name: "Create new",
+            icon: "plus"
         },
         {
-            id: "2",
-            name: "Televisions",
+            name: "Delete ____number____ items",
+            icon: "trash",
             items: [
                 {
                     id: "2_1",
-                    name: "SuperLCD 42",
-                    icon: "images/products/7.png",
+                    name: "Delete",
+                    icon: "trash",
                     price: 1200
                 },
                 {
                     id: "2_2",
-                    name: "SuperLED 42",
-                    icon: "images/products/5.png",
+                    name: "Hard Delete",
+                    icon: "trash",
                     price: 1450
-                },
-                {
-                    id: "2_3",
-                    name: "SuperLED 50",
-                    icon: "images/products/4.png",
-                    price: 1600
-                },
-                {
-                    id: "2_4",
-                    name: "SuperLCD 55 (Not available)",
-                    icon: "images/products/6.png",
-                    price: 1350,
-                    disabled: true
-                },
-                {
-                    id: "2_5",
-                    name: "SuperLCD 70",
-                    icon: "images/products/9.png",
-                    price: 4000
-                }
-            ]
-        },
-        {
-            id: "3",
-            name: "Monitors",
-            items: [
-                {
-                    id: "3_1",
-                    name: '19"',
-                    items: [
-                        {
-                            id: "3_1_1",
-                            name: "DesktopLCD 19",
-                            icon: "images/products/10.png",
-                            price: 160
-                        }
-                    ]
-                },
-                {
-                    id: "3_2",
-                    name: '21"',
-                    items: [
-                        {
-                            id: "3_2_1",
-                            name: "DesktopLCD 21",
-                            icon: "images/products/12.png",
-                            price: 170
-                        },
-                        {
-                            id: "3_2_2",
-                            name: "DesktopLED 21",
-                            icon: "images/products/13.png",
-                            price: 175
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            id: "4",
-            name: "Projectors",
-            items: [
-                {
-                    id: "4_1",
-                    name: "Projector Plus",
-                    icon: "images/products/14.png",
-                    price: 550
-                },
-                {
-                    id: "4_2",
-                    name: "Projector PlusHD",
-                    icon: "images/products/15.png",
-                    price: 750
                 }
             ]
         }
-    ];
+    ].reverse();
 
     // onGridReady(params) {
     //     this.http.get("http://localhost:${process.env.CONTROLLER_PORT}/api/v1/users/").subscribe(data => {
