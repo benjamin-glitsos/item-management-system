e77a8b066642b23c51e4890ee09d97b6630ca3d1
Benjamin Glitsos <bengyup@gmail.com>
2021-01-05 08:57:44 +1100

    Some code cleanup of REPORT method

--- controller-scala/src/main/scala/Directives/CustomMethodDirectives.scala
+++ controller-scala/src/main/scala/Directives/CustomMethodDirectives.scala
@@ -2,5 +2,5 @@ import akka.http.scaladsl.server._
 import akka.http.scaladsl.server.Directives._
 
 object CustomMethodDirectives {
-  def report: Directive0 = method(CustomMethods.REPORT)
+  final val report: Directive0 = method(CustomMethods.REPORT)
 }
