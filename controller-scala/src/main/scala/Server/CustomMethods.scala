a5ed4571ac99859cf94de94b21ac398204b533c1
Benjamin Glitsos <bengyup@gmail.com>
2021-01-05 09:10:07 +1100

    Small CustomMethods refactoring. Untested.

--- controller-scala/src/main/scala/Server/CustomMethods.scala
+++ controller-scala/src/main/scala/Server/CustomMethods.scala
@@ -2,10 +2,12 @@ import akka.http.scaladsl.model.HttpMethod
 import akka.http.scaladsl.model.RequestEntityAcceptance.Expected
 
 object CustomMethods {
-  val REPORT = HttpMethod.custom(
+  final val REPORT = HttpMethod.custom(
     "REPORT",
     safe = true,
     idempotent = true,
     requestEntityAcceptance = Expected
   )
+
+  final def apply() = List[REPORT]
 }
--- controller-scala/src/main/scala/Server/Server.scala
+++ controller-scala/src/main/scala/Server/Server.scala
@@ -17,7 +17,7 @@ object Server {
     val port: Integer = System.getenv("CONTROLLER_PORT").toInt
 
     val parserSettings =
-      ParserSettings.forServer(system).withCustomMethods(CustomMethods.REPORT)
+      ParserSettings.forServer(system).withCustomMethods(CustomMethods())
     val serverSettings =
       ServerSettings(system).withParserSettings(parserSettings)
 
