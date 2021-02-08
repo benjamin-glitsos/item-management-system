fe15cfc194a8069563635afdb20f02de2cd1ed88
Benjamin Glitsos <bengyup@gmail.com>
2021-02-09 09:29:51 +1100

    Rewording the logic inside the admin entrypoint

--- admin-angular/docker-entrypoint.sh
+++ admin-angular/docker-entrypoint.sh
@@ -2,16 +2,10 @@
 
 set -e
 
-if [[ "$PROJECT_DEMO_MODE" == "yes" ]]; then
-    npm build
-else
-    npm build:prod
-fi
-
 if [[ "$ADMIN_RUN" == "yes" ]]; then
     if [[ "$PROJECT_DEMO_MODE" == "yes" ]]; then
         npm start
     else
-        npm start:prod
+        npm build:prod && npm start:prod
     fi
 fi
