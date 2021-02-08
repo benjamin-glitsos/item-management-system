db47f7200d363ff64a110bc2c2c69475cbad5292
Benjamin Glitsos <bengyup@gmail.com>
2021-02-09 10:35:15 +1100

    Improved env file further by using PROJECT_MODE and also formatting it

--- admin-angular/docker-entrypoint.sh
+++ admin-angular/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [[ "$ADMIN_RUN" == "yes" ]]; then
-    if [[ "$PROJECT_DEV_MODE" == "yes" ]]; then
+    if [[ "$PROJECT_MODE" == "development" ]]; then
         npm start
     else
         npm build:prod && npm start:prod
