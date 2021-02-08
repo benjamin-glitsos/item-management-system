abc21284c7f4e8aab526fef6c01909f7d00b8d7f
Benjamin Glitsos <bengyup@gmail.com>
2021-02-09 10:27:06 +1100

    Renamed an env var

--- admin-angular/docker-entrypoint.sh
+++ admin-angular/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [[ "$ADMIN_RUN" == "yes" ]]; then
-    if [[ "$PROJECT_DEMO_MODE" == "yes" ]]; then
+    if [[ "$PROJECT_DEV_MODE" == "yes" ]]; then
         npm start
     else
         npm build:prod && npm start:prod
