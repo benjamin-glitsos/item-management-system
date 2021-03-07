a609a07ca3809035f1f8cc1779c0819fbc5fde2b
Benjamin Glitsos <bengyup@gmail.com>
2021-03-07 13:14:03 +1100

    Numbers to words package now working

--- admin-react/src/components/TableStatusBar.js
+++ admin-react/src/components/TableStatusBar.js
@@ -1,7 +1,12 @@
+import numbersToWords from "number-to-words";
+import capitalise from "../utilities/capitalise";
+
 export default ({ placeholder, numberOfSelected }) => {
     if (numberOfSelected === 0) {
         return placeholder;
     } else {
-        return `${numberOfSelected} selected.`;
+        return `${capitalise(
+            numbersToWords.toWords(numberOfSelected)
+        )} selected.`;
     }
 };
