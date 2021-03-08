5cb643299589b32803c31bc6c3539f34ab40f4a8
Benjamin Glitsos <bengyup@gmail.com>
2021-03-08 19:49:41 +1100

    Minor refactor of friendlyName code

--- admin-react/src/utilities/friendlyName.js
+++ admin-react/src/utilities/friendlyName.js
@@ -3,5 +3,7 @@ export default (firstName, lastName, otherNames) => {
         otherNames &&
         otherNames.split(/\s*/).map(name => name.charAt(0).toUppercase() + ".");
 
-    return [firstName, otherNameInitials, lastName].filter(x => !!x).join(" ");
+    return [firstName, ...otherNameInitials, lastName]
+        .filter(x => !!x)
+        .join(" ");
 };
