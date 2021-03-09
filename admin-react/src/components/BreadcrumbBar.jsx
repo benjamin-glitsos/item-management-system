16ff4a39c1247d7cec58a3415bf411642366d36d
Benjamin Glitsos <bengyup@gmail.com>
2021-03-09 21:01:31 +1100

    Made cursor not change when hovering over breadcrumb bar last item

--- admin-react/src/components/BreadcrumbBar.jsx
+++ admin-react/src/components/BreadcrumbBar.jsx
@@ -1,14 +1,23 @@
 import Breadcrumbs, { BreadcrumbsItem } from "@atlaskit/breadcrumbs";
+import styled from "styled-components";
 
 export default ({ breadcrumbs }) => (
-    <Breadcrumbs>
-        {breadcrumbs.map(([title, path], i) => (
-            <BreadcrumbsItem
-                text={title}
-                key={`BreadcrumbBar/${i}-${title}`}
-                href={path}
-                isDisabled={i + 1 === breadcrumbs.length}
-            />
-        ))}
-    </Breadcrumbs>
+    <DisabledStyles>
+         <Breadcrumbs>
+             {breadcrumbs.map(([title, path], i) => (
+                 <BreadcrumbsItem
+                     text={title}
+                     key={`BreadcrumbBar/${i}-${title}`}
+                     href={path}
+                     isDisabled={i + 1 === breadcrumbs.length}
+                 />
+             ))}
+         </Breadcrumbs>
+    </DisabledStyles>
 );
+
+const DisabledStyles = styled.div`
+    a[disabled] {
+        cursor: default;
+    }
+`;
