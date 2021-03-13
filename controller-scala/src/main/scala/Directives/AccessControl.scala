d83400cf76aed24ca243c7267a4747c57c88eb61
Benjamin Glitsos <bengyup@gmail.com>
2021-03-14 10:01:36 +1100

    Configuring ports for deploying to production

--- controller-scala/src/main/scala/Directives/AccessControl.scala
+++ controller-scala/src/main/scala/Directives/AccessControl.scala
@@ -3,14 +3,20 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.model.headers.RawHeader
 
 object AccessControl {
-  def apply(): Directive0 = respondWithDefaultHeaders(
-    Seq(
-      RawHeader("Access-Control-Allow-Origin", "http://localhost"),
-      RawHeader(
-        "Access-Control-Allow-Methods",
-        "OPTIONS, GET, POST, DELETE, PATCH, REPORT"
-      ),
-      RawHeader("Access-Control-Allow-Headers", "*")
+  def apply(): Directive0 = {
+    val adminPort = System.getenv("ADMIN_PORT")
+    respondWithDefaultHeaders(
+      Seq(
+        RawHeader(
+          "Access-Control-Allow-Origin",
+          s"http://localhost:$adminPort"
+        ),
+        RawHeader(
+          "Access-Control-Allow-Methods",
+          "OPTIONS, GET, POST, DELETE, PATCH, REPORT"
+        ),
+        RawHeader("Access-Control-Allow-Headers", "*")
+      )
     )
-  )
+  }
 }
