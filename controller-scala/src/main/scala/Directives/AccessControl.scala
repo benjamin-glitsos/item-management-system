622c35924e767117aa1bed2eb7a2fc79dbe19617
Benjamin Glitsos <bengyup@gmail.com>
2021-03-14 11:51:29 +1100

    Fixed syntax error

--- controller-scala/src/main/scala/Directives/AccessControl.scala
+++ controller-scala/src/main/scala/Directives/AccessControl.scala
@@ -4,7 +4,7 @@ import akka.http.scaladsl.model.headers.RawHeader
 
 object AccessControl {
   def apply(): Directive0 = {
-    val allowOriginUrl = if (System.getenv("PROJECT_MODE") === "production") {
+    val allowOriginUrl = if (System.getenv("PROJECT_MODE") == "production") {
       val adminDomain = System.getenv("PROJECT_DOMAIN")
       s"https://$adminDomain"
     } else {
