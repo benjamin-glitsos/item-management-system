a6027e3d203fcd79645d111f5457a4632cdb3a8a
Benjamin Glitsos <bengyup@gmail.com>
2021-03-20 12:28:35 +1100

    Improved Article Layout

--- admin-react/src/presenters/ArticleLayout.jsx
+++ admin-react/src/presenters/ArticleLayout.jsx
@@ -1,5 +1,12 @@
+import PageHeader from "@atlaskit/page-header";
+import BreadcrumbBar from "%/presenters/BreadcrumbBar";
 import ContentMargins from "%/presenters/ContentMargins";
 
-export default ({ children }) => (
-    <ContentMargins maxWidth="800px">{children}</ContentMargins>
+export default ({ title, breadcrumbs, children }) => (
+    <ContentMargins maxWidth="800px">
+        <PageHeader breadcrumbs={<BreadcrumbBar breadcrumbs={breadcrumbs} />}>
+            {title}
+        </PageHeader>
+        {children}
+    </ContentMargins>
 );
--- admin-react/src/presenters/ReadmePagePresenter.jsx
+++ admin-react/src/presenters/ReadmePagePresenter.jsx
@@ -1,19 +1,12 @@
 import ReactMarkdown from "react-markdown";
-import PageHeader from "@atlaskit/page-header";
 import readme from "%/assets/README.md";
 import PageLayout from "%/presenters/PageLayout";
 import ArticleLayout from "%/presenters/ArticleLayout";
-import BreadcrumbBar from "%/presenters/BreadcrumbBar";
 import GithubButton from "%/presenters/GithubButton";
 
 export default ({ title, description }) => (
     <PageLayout title={title} description={description}>
-        <ArticleLayout>
-            <PageHeader
-                breadcrumbs={<BreadcrumbBar breadcrumbs={[["Home", "/"]]} />}
-            >
-                {title}
-            </PageHeader>
+        <ArticleLayout title={title} breadcrumbs={[["Home", "/"]]}>
             <ReactMarkdown>{readme}</ReactMarkdown>
             <GithubButton />
         </ArticleLayout>
