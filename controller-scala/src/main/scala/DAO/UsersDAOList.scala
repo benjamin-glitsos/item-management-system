174230b2ab838777e9b68cd0f699fd84d1bcc715
Benjamin Glitsos <bengyup@gmail.com>
2021-03-26 22:48:08 +1100

    Some issues resolved

--- controller-scala/src/main/scala/DAO/UsersDAOList.scala
+++ controller-scala/src/main/scala/DAO/UsersDAOList.scala
@@ -16,24 +16,27 @@ trait UsersDAOList {
     |   , edited_at
     """.stripMargin
 
-    val from: Fragment = fr"users_with_meta"
+    val from: Fragment = fr"FROM users_with_meta"
 
-    val notDeleted = fr"is_deleted=no"
+    // val notDeleted = fr"is_deleted=no"
+    //
+    // val isInSearch = fr"""
+    // | (username LIKE "%$search%")
+    // | OR (email_address LIKE "%$search%")
+    // | OR (first_name LIKE "%$search%")
+    // | OR (last_name LIKE "%$search%")
+    // | OR (other_names LIKE "%$search%")
+    // """
+    //
+    // val where: Fragment = whereAnd(notDeleted, isInSearch)
+    //
+    // val sort: Fragment = fr"ORDER BY edited_at DESC"
+    //
+    // val page: Fragment = fr"LIMIT $pageLength OFFSET $offset"
 
-    val isInSearch = fr"""
-    | (username LIKE "%$search%")
-    | OR (email_address LIKE "%$search%")
-    | OR (first_name LIKE "%$search%")
-    | OR (last_name LIKE "%$search%")
-    | OR (other_names LIKE "%$search%")
-    """
-
-    val where: Fragment = whereAnd(notDeleted, isInSearch)
-
-    val sort: Fragment = fr"ORDER BY edited_at DESC"
-
-    val page: Fragment = fr"LIMIT $pageLength OFFSET $offset"
-
-    (select ++ from).update.run
+    (select ++ from)
+      .query[(String, String, String, List[String], String, List[String])]
+      .compile
+      .toList
   }
 }
