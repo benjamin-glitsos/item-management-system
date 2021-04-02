394ca11fc93f6bc3aefe5a53c91c7d720b03bf92
Benjamin Glitsos <bengyup@gmail.com>
2021-04-02 22:12:37 +1100

    Minor formatting

--- database-postgresql/src/tables/users.sql
+++ database-postgresql/src/tables/users.sql
@@ -1,11 +1,11 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY
-    , username VARCHAR(20) UNIQUE NOT NULL
-    , email_address EMAIL_ADDRESS NOT NULL
-    , first_name VARCHAR(50) NOT NULL
-    , last_name VARCHAR(70) NOT NULL
-    , other_names VARCHAR(120)
-    , password SHA1 NOT NULL
-    , meta_id SMALLINT UNIQUE NOT NULL
-    , CONSTRAINT fk_meta FOREIGN KEY(meta_id) REFERENCES meta(id)
+  , username VARCHAR(20) UNIQUE NOT NULL
+  , email_address EMAIL_ADDRESS NOT NULL
+  , first_name VARCHAR(50) NOT NULL
+  , last_name VARCHAR(70) NOT NULL
+  , other_names VARCHAR(120)
+  , password SHA1 NOT NULL
+  , meta_id SMALLINT UNIQUE NOT NULL
+  , CONSTRAINT fk_meta FOREIGN KEY(meta_id) REFERENCES meta(id)
 );
