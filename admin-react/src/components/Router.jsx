be2e598a2423077816331e21440ac89cede5bff7
Benjamin Glitsos <bengyup@gmail.com>
2021-04-03 23:33:24 +1100

    Code splitting is working

--- admin-react/src/components/Router.jsx
+++ admin-react/src/components/Router.jsx
@@ -1,23 +1,34 @@
+import { Suspense, lazy } from "react";
 import { Router, Route, Switch } from "react-router";
 import { createBrowserHistory } from "history";
 import { QueryParamProvider } from "use-query-params";
 import Analytics from "react-router-ga";
 import Page from "%/presenters/PagePresenter";
-import ReadmePage from "%/components/ReadmePage";
-import UsersPage from "%/components/UsersPage";
+
+const ReadmePage = lazy(() => import("%/components/ReadmePage"));
+const UsersPage = lazy(() => import("%/components/UsersPage"));
 
 export default () => (
     <Router history={createBrowserHistory()}>
         <Page>
             <Analytics id={process.env.REACT_APP_PROJECT_GOOGLE_ANALYTICS_ID}>
-                <Switch>
-                    <QueryParamProvider ReactRouterRoute={Route} exact path="/">
-                        <ReadmePage />
-                    </QueryParamProvider>
-                    <QueryParamProvider ReactRouterRoute={Route} path="/users">
-                        <UsersPage />
-                    </QueryParamProvider>
-                </Switch>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Switch>
+                        <QueryParamProvider
+                            ReactRouterRoute={Route}
+                            exact
+                            path="/"
+                        >
+                            <ReadmePage />
+                        </QueryParamProvider>
+                        <QueryParamProvider
+                            ReactRouterRoute={Route}
+                            path="/users"
+                        >
+                            <UsersPage />
+                        </QueryParamProvider>
+                    </Switch>
+                </Suspense>
             </Analytics>
         </Page>
     </Router>
