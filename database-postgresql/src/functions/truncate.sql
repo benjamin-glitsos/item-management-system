937882bcda50fc675830000580920cf37de46901
Benjamin Glitsos <bengyup@gmail.com>
2021-04-04 20:28:42 +1000

    Working on the truncate function

--- database-postgresql/src/functions/truncate.sql
+++ database-postgresql/src/functions/truncate.sql
@@ -1,6 +1,12 @@
-CREATE FUNCTION truncate(n integer, s text)
+CREATE FUNCTION truncate(max_length integer, full_text text)
 RETURNS text
 AS $$
-    SELECT LEFT(s, n - 3) || '...'
-    -- TODO: use if/else to add ellipsis only if it exceeds (n - 3) and only if string is not null. Use declaring variables to make this neater.
+DECLARE
+    ellipsis text := '...';
+    ellipsis_length integer := 3
+BEGIN
+    RETURN LEFT(full_text, max_length - ellipsis_length) || ellipsis
+END;
 $$ LANGUAGE plpgsql;
+
+-- TODO: use if/else to add ellipsis only if it exceeds (n - 3) and only if string is not null. Use declaring variables to make this neater.
