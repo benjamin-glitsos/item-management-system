2918f9ceac7ca570c9625021fb71ce6fa9baf4be
Benjamin Glitsos <bengyup@gmail.com>
2021-04-04 12:00:36 +1000

    Added items views

---
+++ database-postgresql/src/views/items-list.sql
@@ -0,0 +1,8 @@
+CREATE VIEW items_list AS
+    SELECT
+        name
+      , CONCAT(LEFT(description, 50), IF(LENGTH(description) > 50, '...', ''))
+      , created_at
+      , edited_at
+    FROM items_with_meta
+    WHERE is_deleted = false;
---
+++ database-postgresql/src/views/items-with-meta.sql
@@ -0,0 +1,14 @@
+CREATE VIEW items_with_meta AS
+    SELECT
+        i.name
+      , i.description
+      , m.metakey
+      , m.opens
+      , m.edits
+      , m.is_deleted
+      , m.created_at
+      , m.edited_at
+      , m.deleted_at
+      , m.notes
+    FROM items i
+    INNER JOIN meta m ON i.meta_id = m.id;
