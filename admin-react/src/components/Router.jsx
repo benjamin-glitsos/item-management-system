1041d2abffffe34866a52afaa6da2955e036bf09
Benjamin Glitsos <bengyup@gmail.com>
2021-04-10 10:52:33 +1000

    The lightbox styling isnt perfect yet but it is good enough for now

--- admin-react/src/components/Router.jsx
+++ admin-react/src/components/Router.jsx
@@ -1,5 +1,4 @@
 import { Suspense, lazy } from "react";
-import styled from "styled-components";
 import { Router, Route, Switch } from "react-router";
 import { createBrowserHistory } from "history";
 import { QueryParamProvider } from "use-query-params";
@@ -13,42 +12,32 @@ const NotFoundPage = lazy(() => import("%/components/NotFoundPage"));
 
 export default () => (
     <Router history={createBrowserHistory()}>
-        <ZIndexStyles>
-            <Page>
-                <Analytics
-                    id={process.env.REACT_APP_PROJECT_GOOGLE_ANALYTICS_ID}
-                >
-                    <Suspense fallback={<LoadingPage />}>
-                        <Switch>
-                            <QueryParamProvider
-                                ReactRouterRoute={Route}
-                                exact
-                                path="/"
-                            >
-                                <ReadmePage />
-                            </QueryParamProvider>
-                            <QueryParamProvider
-                                ReactRouterRoute={Route}
-                                path="/users"
-                            >
-                                <UsersPage />
-                            </QueryParamProvider>
-                            <QueryParamProvider
-                                ReactRouterRoute={NotFoundPage}
-                                path="*"
-                            >
-                                <NotFoundPage />
-                            </QueryParamProvider>
-                        </Switch>
-                    </Suspense>
-                </Analytics>
-            </Page>
-        </ZIndexStyles>
+        <Page>
+            <Analytics id={process.env.REACT_APP_PROJECT_GOOGLE_ANALYTICS_ID}>
+                <Suspense fallback={<LoadingPage />}>
+                    <Switch>
+                        <QueryParamProvider
+                            ReactRouterRoute={Route}
+                            exact
+                            path="/"
+                        >
+                            <ReadmePage />
+                        </QueryParamProvider>
+                        <QueryParamProvider
+                            ReactRouterRoute={Route}
+                            path="/users"
+                        >
+                            <UsersPage />
+                        </QueryParamProvider>
+                        <QueryParamProvider
+                            ReactRouterRoute={NotFoundPage}
+                            path="*"
+                        >
+                            <NotFoundPage />
+                        </QueryParamProvider>
+                    </Switch>
+                </Suspense>
+            </Analytics>
+        </Page>
     </Router>
 );
-
-const ZIndexStyles = styled.div`
-    div > div > div:nth-child(2) {
-        z-index: 3;
-    }
-`;
