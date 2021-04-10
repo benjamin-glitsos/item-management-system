1255007be4af0c3a555a1d0366426fdab2c08e0b
Benjamin Glitsos <bengyup@gmail.com>
2021-04-11 05:59:40 +1000

    Combined UsersPage into a single component rather than Container-Presenter pattern

--- admin-react/src/components/UsersPage.jsx
+++ admin-react/src/components/UsersPage.jsx
@@ -1,13 +1,82 @@
-import { createContext } from "react";
-import UsersPageContainer from "%/containers/UsersPageContainer";
-import UsersPagePresenter from "%/presenters/UsersPagePresenter";
+import { createContext, useContext } from "react";
+import { titleCase } from "title-case";
+import PageContainer from "%/containers/PageContainer";
+import ListContainer from "%/containers/ListContainer";
+import PageLayout from "%/presenters/PageLayout";
+import List from "%/components/List";
+import fromMaybe from "%/utilities/fromMaybe";
+import friendlyDate from "%/utilities/friendlyDate";
 
-export const UsersContext = createContext();
+export const Context = createContext();
 
-const { Provider } = UsersContext;
+const { Provider } = Context;
 
-export default () => (
-    <Provider value={UsersPageContainer()}>
-        <UsersPagePresenter />
-    </Provider>
-);
+export default () => {
+    const context = useContext(Context);
+
+    const nameSingular = "user";
+    const namePlural = "users";
+    const title = titleCase(namePlural);
+    const slug = namePlural;
+
+    const pageContainer = PageContainer({
+        nameSingular,
+        namePlural,
+        title,
+        slug,
+        namePlural,
+        description: `A list of users who can log into the ${
+            process.env.PROJECT_NAME || "Item Management System"
+        }.`
+    });
+
+    const listContainer = ListContainer({
+        apiPath: `v1/${slug}/`,
+        defaultState: {
+            request: {
+                body: {}
+            },
+            response: {
+                data: {
+                    items: []
+                },
+                errors: []
+            },
+            isLoading: false,
+            selected: []
+        },
+        headContentColumns: [
+            { key: "username", content: "Username", isSortable: true },
+            { key: "first_name", content: "Name", isSortable: true },
+            {
+                key: "email_address",
+                content: "Email Address",
+                isSortable: true
+            }
+        ],
+        rowTransform: row =>
+            transform(row, {
+                created_at: friendlyDate,
+                edited_at: d => friendlyDate(fromMaybe(d))
+            })
+    });
+
+    const usersPageContext = {
+        nameSingular,
+        namePlural,
+        title,
+        ...pageContainer,
+        ...listContainer
+    };
+
+    return (
+        <Provider value={usersPageContext}>
+            <PageLayout
+                title={usersPageContext.metaTitle}
+                description={usersPageContext.description}
+            >
+                <List context={usersPageContext} />
+            </PageLayout>
+        </Provider>
+    );
+};
