4c4baac9e89d4a17611fc46c0151097bc3adb1c3
Benjamin Glitsos <bengyup@gmail.com>
2021-04-11 07:16:19 +1000

    Started adding check constraints to the database to prevent empty strings

--- database-postgresql/src/tables/users.sql
+++ database-postgresql/src/tables/users.sql
@@ -1,10 +1,10 @@
 CREATE TABLE users (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY
-  , username USERNAME UNIQUE NOT NULL
-  , email_address EMAIL_ADDRESS NOT NULL
-  , first_name VARCHAR(50) NOT NULL
-  , last_name VARCHAR(70) NOT NULL
-  , other_names VARCHAR(120)
-  , password SHA1 NOT NULL
+  , username USERNAME UNIQUE NOT NULL CHECK (username <> '')
+  , email_address EMAIL_ADDRESS NOT NULL CHECK (email_address <> '')
+  , first_name VARCHAR(50) NOT NULL CHECK (first_name <> '')
+  , last_name VARCHAR(70) NOT NULL CHECK (last_name <> '')
+  , other_names VARCHAR(120) CHECK ('' <> ANY(other_names))
+  , password SHA1 NOT NULL CHECK (password <> '')
   , meta_id SMALLINT UNIQUE NOT NULL REFERENCES meta(id)
 );
