2f7401ce8d069522bfcb1efb2a3f002d4a4841ca
Benjamin Glitsos <bengyup@gmail.com>
2021-04-14 06:27:03 +1000

    Added a todo

--- admin-react/src/components/UsersOpenPage.jsx
+++ admin-react/src/components/UsersOpenPage.jsx
@@ -2,5 +2,7 @@ import { useParams } from "react-router-dom";
 
 export default () => {
     const { username } = useParams();
+    // TODO: First check that every feature will work
+    // TODO: Start with json-schema-to-yup
     return `The username is: ${username}`;
 };
