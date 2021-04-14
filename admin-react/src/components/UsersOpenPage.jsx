5a3f0f00b7effbe0afc0ff14b095474c85eb15f5
Benjamin Glitsos <bengyup@gmail.com>
2021-04-14 17:50:14 +1000

    Removed joi

--- admin-react/src/components/UsersOpenPage.jsx
+++ admin-react/src/components/UsersOpenPage.jsx
@@ -4,14 +4,10 @@ import { useImmer } from "use-immer";
 import { useForm } from "react-hook-form";
 import { buildYup } from "json-schema-to-yup";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { joiResolver } from "@hookform/resolvers/joi";
 import * as Yup from "yup";
-import Joi from "joi";
-import Enjoi from "enjoi";
 
 export default () => {
     // TODO: First check that every feature will work
-    // TODO NEXT: use buildYup to use your state.schema in the form
     // TODO NEXT: create the schema/edit-users endpoint and ensure that it pulls in the ref fields properly
     // TODO: add functionality to onSubmit that removes attributes that are blank strings or maybe also other blank properties e.g. []
 
@@ -25,11 +21,16 @@ export default () => {
             properties: {
                 username: {
                     description: "E.g. bdole43",
-                    type: "string"
+                    type: "string",
+                    maxLength: 20,
+                    pattern: "^[-_a-zA-Z0-9]*$"
                 },
                 first_name: {
                     description: "E.g. Bob",
-                    type: "string"
+                    type: "string",
+                    minLength: 1,
+                    maxLength: 50,
+                    pattern: "^[^\\s]+(s+[^\\s]+)*$"
                 }
             },
             minProperties: 1
@@ -39,7 +40,7 @@ export default () => {
 
     const [state, setState] = useImmer(defaultState);
 
-    const apiUrl = "/users/benglitsos";
+    const apiUrl = `/users/${username}`;
 
     const requestItem = () =>
         axios({
@@ -68,16 +69,6 @@ export default () => {
 
     useEffect(getItemAction, []);
 
-    Yup.addMethod(Yup.object, "atLeastOneOf", function (list) {
-        return this.test({
-            name: "atLeastOneOf",
-            message: "${path} must have at least one of these keys: ${keys}",
-            exclusive: true,
-            params: { keys: list.join(", ") },
-            test: value => value == null || list.some(f => value[f] != null)
-        });
-    });
-
     const yupSchemaExample = Yup.object().shape({
         username: Yup.string(),
         first_name: Yup.string()
@@ -85,20 +76,12 @@ export default () => {
 
     const yupSchema = buildYup(state.schema, {});
 
-    const joiSchemaExample = Joi.object({
-        username: Joi.string().optional().allow(""),
-        first_name: Joi.string().optional().allow("")
-    }).min(3);
-
-    const joiSchema = Enjoi.schema(state.schema);
-
     const {
         register,
         handleSubmit,
         formState: { errors }
     } = useForm({
         resolver: yupResolver(yupSchema)
-        // resolver: joiResolver(joiSchemaExample)
     });
 
     const onSubmit = data => console.log(data);
