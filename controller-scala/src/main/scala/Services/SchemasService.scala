177caf975b0cecbdca6124a817bb45c5680ec614
Benjamin Glitsos <bengyup@gmail.com>
2021-04-16 18:05:01 +1000

    Schemas route returning json properly, but not resolving refs yet

--- controller-scala/src/main/scala/Services/SchemasService.scala
+++ controller-scala/src/main/scala/Services/SchemasService.scala
@@ -2,11 +2,11 @@ import upickle.default._
 import upickle_import.general._
 import org.everit.json.schema.{Schema}
 
-object SchemasService {
+object SchemasService extends ServiceTrait {
   private final def load(name: String): Schema = SchemasDAO.load(name: String)
 
   final def loadSchema(name: String): Schema = load(name)
 
-  final def loadJson(name: String): Schema =
+  final def loadJson(name: String): ujson.Value =
     read[ujson.Value](load(name).toString())
 }
