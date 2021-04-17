78daf5bf66261a3ed005e83e63a76960f8edebc9
Benjamin Glitsos <bengyup@gmail.com>
2021-04-17 17:43:12 +1000

    API now returning nulls as intended, but front-end not adapted to use them yet

--- controller-scala/src/main/scala/Imports/UPickleImports.scala
+++ controller-scala/src/main/scala/Imports/UPickleImports.scala
@@ -8,6 +8,20 @@ import org.typelevel.ci.CIString
 
 package upickle_import {
   object general {
+    implicit def OptionWriter[T: Writer]: Writer[Option[T]] =
+      implicitly[Writer[T]].comap[Option[T]] {
+        case None    => null.asInstanceOf[T]
+        case Some(x) => x
+      }
+
+    implicit def OptionReader[T: Reader]: Reader[Option[T]] = {
+      new Reader.Delegate[Any, Option[T]](
+        implicitly[Reader[T]].map(Some(_))
+      ) {
+        override def visitNull(index: Int) = None
+      }
+    }
+
     implicit final val upickleLocalDateTime: ReadWriter[LocalDateTime] =
       readwriter[ujson.Value].bimap[LocalDateTime](
         x => x.toString,
