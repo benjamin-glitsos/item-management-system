214543f07932b4fb518511b1526f0331d442555c
Benjamin Glitsos <bengyup@gmail.com>
2021-04-21 19:01:26 +1000

    Shortened an error message

--- admin-react/src/utilities/noNewDataToSubmitToast.jsx
+++ admin-react/src/utilities/noNewDataToSubmitToast.jsx
@@ -5,7 +5,7 @@ export default () => {
     const message = new Message(
         "no_new_data_to_submit",
         "No new data to submit",
-        "No values in the form are different to the data that is already present on the server, so there is no new data to submit."
+        "No values in the form are different to the data that is already present on the server."
     );
     console.log(message);
     toast(message);
