dc696360b52df19ef842041e52cbda0d74558b3f
Benjamin Glitsos <bengyup@gmail.com>
2021-04-27 09:11:00 +1000

    Removed one console log that was used for testing

--- admin-react/src/utilities/formatAction.jsx
+++ admin-react/src/utilities/formatAction.jsx
@@ -1,5 +1,4 @@
 export default (numberOfItems, title, namePlural) => {
-    console.log(namePlural);
     if (numberOfItems < 1) {
         return `Zero ${namePlural} selected`;
     } else if (numberOfItems == 1) {
