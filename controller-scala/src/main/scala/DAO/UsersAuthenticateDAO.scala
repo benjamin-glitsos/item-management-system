719838713fc973e28e146c9f570a39cf2be871a6
Benjamin Glitsos <bengyup@gmail.com>
2021-06-27 10:25:10 +1000

    Authentication DAO coded but untested

---
+++ controller-scala/src/main/scala/DAO/UsersAuthenticateDAO.scala
@@ -0,0 +1,21 @@
+import doobie_import.database.dc._
+import doobie.Fragment
+import doobie.Fragments.{setOpt, whereAnd}
+import doobie._
+import doobie.implicits._
+
+trait UsersAuthenticateDAO {
+  final def authenticate(
+      username: String,
+      password: String
+  ) = {
+    fr"""
+    SELECT EXISTS (
+      SELECT true FROM users
+      WHERE username = $username
+      AND   password = sha1_encrypt($password)
+    )
+    FROM users
+    """.query[Boolean].head
+  }
+}
