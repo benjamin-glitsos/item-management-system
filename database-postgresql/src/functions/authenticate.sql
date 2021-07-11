d8b8aaa6d81288e3d3f7f8c9f4e15290502cafd4
Benjamin Glitsos <bengyup@gmail.com>
2021-07-11 15:02:59 +1000

    Minor formatting of .env and some todos

--- database-postgresql/src/functions/authenticate.sql
+++ database-postgresql/src/functions/authenticate.sql
@@ -7,3 +7,7 @@ SELECT EXISTS (
 )
 FROM users;
 $$ LANGUAGE sql;
+
+-- TODO: change it to psql language. Use RETURNS query
+-- TODO: return the metakey from users_open and then in the doobie you will need to use ".option" instead of ".unique"
+-- TODO: the if-else will increment either successful_logins field or failed_logins field on users table
