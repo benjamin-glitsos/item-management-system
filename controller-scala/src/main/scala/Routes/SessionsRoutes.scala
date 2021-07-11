a63cded84778c7ef779038a1e85cef2559856fe9
Benjamin Glitsos <bengyup@gmail.com>
2021-07-11 20:02:57 +1000

    Removed the token from the endpoint of routes - not completed yet anyway

--- controller-scala/src/main/scala/Routes/SessionsRoutes.scala
+++ controller-scala/src/main/scala/Routes/SessionsRoutes.scala
@@ -2,19 +2,7 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 
 object SessionsRoutes {
-  private final def rootRoutes(): Route = concat(
-    LoginSessionsRoutes()
-  )
-
-  // private final def sessionTokenRoutes(): Route = pathPrefix(Segment) {
-  //   sessionToken: String =>
-  //     concat(
-  //       LogoutSessionsRoutes(sessionToken)
-  //     )
-  // }
-
   final def apply(): Route = concat(
-    // sessionTokenRoutes(),
-    rootRoutes()
+    LoginSessionsRoutes()
   )
 }
