4cb1a1b19e7d011b514d15640cb5a67105965bee
Benjamin Glitsos <bengyup@gmail.com>
2021-07-11 21:18:02 +1000

    Redis connection refused so far

--- controller-scala/src/main/scala/Mixins/ErrorMixin.scala
+++ controller-scala/src/main/scala/Mixins/ErrorMixin.scala
@@ -2,7 +2,7 @@ import org.typelevel.ci.{CIString => CaseInsensitive}
 import cats.data.NonEmptyChain
 import upickle.default._
 
-trait ErrorMixin {
+trait ErrorMixin extends UpickleMixin {
   final val NOT_FOUND            = CaseInsensitive("not_found")
   final val INVALID_INPUT        = CaseInsensitive("invalid_input")
   final val AUTHORISATION_FAILED = CaseInsensitive("authorisation_failed")
--- controller-scala/src/main/scala/Mixins/RedisMixin.scala
+++ controller-scala/src/main/scala/Mixins/RedisMixin.scala
@@ -1,6 +1,6 @@
 import redis.clients.jedis.Jedis
 
 trait RedisMixin {
-  final val redis = new Jedis("localhost", System.getenv("REDIS_PORT"))
+  final val redis = new Jedis("localhost", System.getenv("REDIS_PORT").toInt)
   redis.auth(System.getenv("REDIS_PASSWORD"))
 }
