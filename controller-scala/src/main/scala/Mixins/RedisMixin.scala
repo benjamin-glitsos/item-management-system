be0a92b57b717d63893b0bfd961f7ffc751f6908
Benjamin Glitsos <bengyup@gmail.com>
2021-07-12 08:36:04 +1000

    Attempting to fix redis connection

--- controller-scala/src/main/scala/Mixins/RedisMixin.scala
+++ controller-scala/src/main/scala/Mixins/RedisMixin.scala
@@ -1,6 +1,9 @@
 import redis.clients.jedis.Jedis
 
 trait RedisMixin {
-  final val redis = new Jedis("localhost", System.getenv("REDIS_PORT").toInt)
-  redis.auth(System.getenv("REDIS_PASSWORD"))
+  final val redis = {
+    val r = new Jedis("localhost", System.getenv("REDIS_PORT").toInt)
+    r.auth(System.getenv("REDIS_PASSWORD"))
+    r
+  }
 }
