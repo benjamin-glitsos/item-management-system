d3186aceeaccd68553d0e0e293f26b9891821fdc
Benjamin Glitsos <bengyup@gmail.com>
2021-07-15 18:16:19 +1000

    Removed the data validation diagram for now because it was the wrong IT diagram type

--- admin-react/src/components/ReadmePage.jsx
+++ admin-react/src/components/ReadmePage.jsx
@@ -44,19 +44,6 @@ export default () => {
                     <ReactMarkdown source={readme} />
                     <h2>Codebase</h2>
                     <GithubButton />
-                    <h2>Data Validation BPMN Diagram</h2>
-                    <DiagramStyles>
-                        <ImagesLightbox
-                            images={[
-                                {
-                                    src:
-                                        process.env.PUBLIC_URL +
-                                        "/images/data-validation-diagram.svg",
-                                    alt: `BPMN diagram of the data validation paths of the ${process.env.PROJECT_ABBREV}`
-                                }
-                            ]}
-                        />
-                    </DiagramStyles>
                     <h2>Database ER Diagram</h2>
                     <DiagramStyles>
                         <ImagesLightbox
