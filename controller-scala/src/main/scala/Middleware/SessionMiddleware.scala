2131bef49c2003d71d9531f51fa88be756cb5feb
Benjamin Glitsos <bengyup@gmail.com>
2021-07-16 19:52:04 +1000

    extractRequest flatMap pass now working

--- controller-scala/src/main/scala/Middleware/SessionMiddleware.scala
+++ controller-scala/src/main/scala/Middleware/SessionMiddleware.scala
@@ -2,13 +2,15 @@ import akka.http.scaladsl.server._
 import akka.http.scaladsl.server.Directives._
 
 object SessionMiddleware extends StringMixin {
-  final def apply(): Directive0[ujson.Value] =
-    extractRequest { request =>
+  final def apply(): Directive0 =
+    extractRequest flatMap { request =>
       {
-        println(request.uri)
-        pass(request)
+        println(request.method.name)
+        println(request.uri.toString)
+        pass
       }
     }
+
   // headerValueByName("Authorization") { authorisationValue =>
   //   {
   //     if (isEmpty(SessionsDAO.get(authorisationToken))) {
@@ -19,3 +21,30 @@ object SessionMiddleware extends StringMixin {
 }
 
 // TODO: the Authorization header will use the "Bearer" type. So it will be like - Authorization: Bearer <authorisation_token>
+
+// import akka.http.scaladsl.server._
+// import akka.http.scaladsl.server.Directives._
+// import scala.concurrent.duration._
+// import akka.http.scaladsl.model.HttpEntity
+// import cats.implicits._
+// import cats.data.Validated.{Valid, Invalid}
+//
+// object SessionMiddleware extends StringMixin {
+//   final def apply(): Directive0 =
+//     extractStrictEntity(3.seconds)
+//       .flatMap((entity: HttpEntity.Strict) => {
+//         if (staticEndpoints contains endpointName) {
+//           provide(ujsonEmptyValue)
+//         } else {
+//           val entityText = entity.data.utf8String
+//
+//           SchemaValidation(endpointName, entityText) match {
+//             case Valid(v) => provide(v)
+//             case Invalid(e) =>
+//               reject(
+//                 ValidationRejection(serialiseErrors(e))
+//               )
+//           }
+//         }
+//       })
+// }
