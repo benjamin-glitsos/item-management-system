20f775b0bbe9d012fa00f604b4ef9199ccd84c9a
Benjamin Glitsos <bengyup@gmail.com>
2021-07-24 01:14:22 +1000

    logout route now working

--- controller-scala/src/main/scala/Routes/LogoutSessionsRoutes.scala
+++ controller-scala/src/main/scala/Routes/LogoutSessionsRoutes.scala
@@ -3,11 +3,11 @@ import akka.http.scaladsl.server.Route
 
 object LogoutSessionsRoutes extends UpickleMixin {
   final def apply(): Route = delete {
-    (SetActionKeyMiddleware("login-sessions") & ValidationMiddleware()) {
-      body: ujson.Value =>
-        headerValueByName("X-Auth-Token") { authenticationToken =>
-          complete(SessionsService.logout(authenticationToken))
-        }
+    (SetActionKeyMiddleware(
+      "login-sessions"
+    ) & ValidationMiddleware() & headerValueByName("X-Auth-Token")) {
+      (body: ujson.Value, authenticationToken: String) =>
+        complete(SessionsService.logout(authenticationToken))
     }
   }
 }
