97502d6548fd0bce35a810b9a523103c27cf9c1b
Benjamin Glitsos <bengyup@gmail.com>
2021-08-28 18:14:45 +1000

    Log request middleware types not working yet

--- controller-scala/src/main/scala/Middleware/LogRequestMiddleware.scala
+++ controller-scala/src/main/scala/Middleware/LogRequestMiddleware.scala
@@ -1,7 +1,8 @@
 import akka.http.scaladsl.server._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.model.HttpRequest
-import java.time.LocalDateTime
+import akka.http.scaladsl.model.HttpEntity
+import scala.concurrent.duration._
 import org.apache.logging.log4j.{LogManager, Logger};
 
 object LogRequestMiddleware
@@ -11,10 +12,16 @@ object LogRequestMiddleware
   private final val log: Logger = LogManager.getLogger("request-logger")
 
   final def apply(): Directive0 =
-    mapRequest((req: HttpRequest) => {
-      log.info(
-        s"${req.method.name} ${req.uri} ${req.protocol.value} ${req.getHeader("X-Action-Key")}"
-      )
-      req
-    })
+    extractStrictEntity(3.seconds) { entity: HttpEntity.Strict =>
+      extractRequest { req: HttpRequest =>
+        log.info(
+          List(
+            req.hashCode,
+            req.method.name,
+            req.uri.path,
+            entity.data.utf8String
+          ).mkString(" ")
+        )
+      }
+    }
 }
