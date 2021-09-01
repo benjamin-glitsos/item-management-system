cc0946585f1b1a54132aaa5798a960529cb0a0c2
Benjamin Glitsos <bengyup@gmail.com>
2021-09-01 21:29:48 +1000

    Refactored some controller code

--- controller-scala/src/main/scala/Server.scala
+++ controller-scala/src/main/scala/Server.scala
@@ -11,41 +11,35 @@ import org.fusesource.jansi.Ansi._
 import org.fusesource.jansi.Ansi.Color._
 
 object Server extends SessionMixin with HttpMethodsMixin {
+  implicit val system           = ActorSystem(Behaviors.empty, "actor-system")
+  implicit val executionContext = system.executionContext
+
   AnsiConsole.systemInstall();
 
   final val apiUri: String =
     s"http://localhost:${System.getenv("CONTROLLER_PORT")}"
   final val adminUri: String =
     s"http://localhost:${System.getenv("ADMIN_PORT")}"
+  final val host: String  = "controller-scala"
+  final val port: Integer = System.getenv("CONTROLLER_PORT").toInt
+
+  private val parserSettings: ParserSettings =
+    ParserSettings
+      .forServer(system)
+      .withCustomMethods(allCustomHttpMethods: _*)
+
+  private val serverSettings: ServerSettings =
+    ServerSettings(system).withParserSettings(parserSettings)
 
   final def apply(): Unit = {
-    implicit val system           = ActorSystem(Behaviors.empty, "actor-system")
-    implicit val executionContext = system.executionContext
-
-    val host: String  = "controller-scala"
-    val port: Integer = System.getenv("CONTROLLER_PORT").toInt
-
-    val parserSettings =
-      ParserSettings
-        .forServer(system)
-        .withCustomMethods(allCustomHttpMethods: _*)
-    val serverSettings =
-      ServerSettings(system).withParserSettings(parserSettings)
-
-    val bindingFuture: Future[ServerBinding] =
-      Http()
-        .newServerAt(host, port)
-        .withSettings(serverSettings)
-        .bind(Routes())
+    Http()
+      .newServerAt(host, port)
+      .withSettings(serverSettings)
+      .bind(Routes())
 
     System.out.println(
       ansi()
-        .a("\n")
         .a(FilesDAO.openPrivateFile("resources/banner.txt"))
-        .fg(BLUE)
-        .a(System.getenv("PROJECT_NAME").toUpperCase)
-        .a("\n")
-        .a("\n")
         .fg(BLUE)
         .a("The API is online at ")
         .bold()
@@ -56,39 +50,6 @@ object Server extends SessionMixin with HttpMethodsMixin {
         .bold()
         .a(s"$adminUri/")
         .reset()
-        .a("\n")
-    )
-
-    var terminal_input: String = new String
-    do {
-      if (terminal_input != new String && terminal_input != "exit") {
-        println(s"The command '$terminal_input' does not exist")
-      }
-
-      terminal_input = StdIn
-        .readLine(
-          ansi()
-            .fg(BLUE)
-            .a("Use 'exit' to shutdown the server... ")
-            .a("\n")
-            .reset()
-            .toString
-        )
-        .stripMargin
-        .stripLineEnd
-    } while (terminal_input != "exit")
-
-    bindingFuture
-      .flatMap(_.unbind())
-      .onComplete(_ => system.terminate())
-
-    System.out.println(
-      ansi()
-        .a("\n")
-        .fg(BLUE)
-        .a("Server shutdown complete.")
-        .reset()
-        .a("\n")
     )
   }
 }
