e3055bfb8d87a983397d7145f185b32c0f187c6f
Benjamin Glitsos <bengyup@gmail.com>
2021-09-17 20:20:59 +1000

    Resolver is almost finished

--- admin-react/src/hooks/useYupSchemaResolver.js
+++ admin-react/src/hooks/useYupSchemaResolver.js
@@ -1,7 +1,7 @@
 import { useCallback } from "react";
 import R from "ramda";
 import * as Yup from "yup";
-import { buildYup as schemaToYup } from "schema-to-yup";
+import { buildYup as jsonSchemaToYup } from "schema-to-yup";
 import { diff } from "deep-object-diff";
 import isObjectEmpty from "%/utilities/isObjectEmpty";
 import mapObjKeys from "%/utilities/mapObjKeys";
@@ -20,8 +20,6 @@ const emptySchema = {
 const yupConfig = {
     abortEarly: false
 };
-
-const schemaToYupConfig = {};
 
 export default ({ schemaData, queryData }) => {
     const schema = getQueryData(schemaData) || emptySchema;
@@ -61,7 +59,40 @@ export default ({ schemaData, queryData }) => {
         return sc;
     })();
 
-    const yupSchema = schemaToYup(cleanSchema, schemaToYupConfig);
+    const jsonSchemaToYupConfig = {
+        errMessages: (() => {
+            const descriptionAttributePattern = /^(?<fieldName>.*)Description$/;
+
+            return Object.fromEntries(
+                Object.entries(schema?.properties || []).reduce(
+                    (accumulator, current) => {
+                        const [key, attributes] = current;
+
+                        const descriptionAttributes = R.pipe(
+                            R.pickBy((value, key) =>
+                                key.match(descriptionAttributePattern)
+                            ),
+                            mapObjKeys(
+                                key =>
+                                    descriptionAttributePattern.exec(key)
+                                        ?.groups?.fieldName
+                            )
+                        )(attributes);
+
+                        const attributesPair =
+                            Object.keys(descriptionAttributes).length > 0
+                                ? [[key, descriptionAttributes]]
+                                : [];
+
+                        return [...accumulator, ...attributesPair];
+                    },
+                    []
+                )
+            );
+        })()
+    };
+
+    const yupSchema = jsonSchemaToYup(cleanSchema, jsonSchemaToYupConfig);
 
     return useCallback(
         async data => {
@@ -81,130 +112,32 @@ export default ({ schemaData, queryData }) => {
                     errors: {}
                 };
             } catch (errors) {
-                return {
-                    values: {},
-                    errors: errors.inner.reduce(
-                        (allErrors, currentError) => ({
-                            ...allErrors,
-                            [currentError.path]: {
-                                type: currentError.type ?? "validation",
-                                message: currentError.message
+                const cleanErrors = R.pipe(
+                    errors =>
+                        errors.inner.map(error => {
+                            const { message, path, type, value } = error;
+                            if (type === "typeError") {
+                                return new Yup.ValidationError(
+                                    [`${path} is required`],
+                                    value,
+                                    path
+                                );
+                            } else {
+                                return error;
                             }
                         }),
-                        {}
-                    )
+                    R.map(e => ({ [e.path]: e.message })),
+                    R.chain(R.toPairs),
+                    R.groupBy(R.head),
+                    R.map(R.pluck(1))
+                )(errors);
+
+                return {
+                    values: {},
+                    errors: cleanErrors
                 };
             }
         },
         [yupSchema]
     );
 };
-
-// export default ({ jsonSchema = {}, data = {}, useFormProps = {} }) => {
-//     const getFormFields = schemaProperties => {
-//         if (!schemaProperties) {
-//             return [];
-//         } else {
-//             return Object.keys(schemaProperties);
-//         }
-//     };
-//
-//     const getErrMessagesFromSchema = () => {
-//         const descriptionAttributePattern = /^(?<fieldName>.*)Description$/;
-//         return Object.fromEntries(
-//             Object.entries(jsonSchema?.properties || []).reduce(
-//                 (accumulator, current) => {
-//                     const [key, attributes] = current;
-//
-//                     const descriptionAttributes = R.pipe(
-//                         R.pickBy((value, key) =>
-//                             key.match(descriptionAttributePattern)
-//                         ),
-//                         mapObjKeys(
-//                             key =>
-//                                 descriptionAttributePattern.exec(key)?.groups
-//                                     ?.fieldName
-//                         )
-//                     )(attributes);
-//
-//                     const attributesPair =
-//                         Object.keys(descriptionAttributes).length > 0
-//                             ? [[key, descriptionAttributes]]
-//                             : [];
-//
-//                     return [...accumulator, ...attributesPair];
-//                 },
-//                 []
-//             )
-//         );
-//     };
-//
-//     const jsonSchemaToYupConfig = {
-//         errMessages: {
-//             ...getErrMessagesFromSchema()
-//         }
-//     };
-//
-//     const yupSchema = isObjectEmpty(jsonSchema)
-//         ? Yup.object()
-//         : jsonSchemaToYup(jsonSchema, jsonSchemaToYupConfig);
-//
-//     const formatYupErrors = R.pipe(
-//         R.map(e => ({ [e.path]: e.message })),
-//         R.chain(R.toPairs),
-//         R.groupBy(R.head),
-//         R.map(R.pluck(1))
-//     );
-//
-//     function resolver() {
-//         useCallback(
-//             async x => {
-//                 const formattedData = R.pipe(
-//                     x => trimAll(x),
-//                     x => emptyStringsToNull(x),
-//                     x => removeAllUndefined(x),
-//                     x => diff(data),
-//                     R.pick(getFormFields(jsonSchema?.properties))
-//                 )(x);
-//
-//                 class Output {
-//                     constructor(values = {}, errors = {}) {
-//                         this.values = values;
-//                         this.errors = errors;
-//                     }
-//                 }
-//
-//                 try {
-//                     const values = await yupSchema.validate(
-//                         formattedData,
-//                         yupConfig
-//                     );
-//                     const formattedValues = emptyStringsToNull(values);
-//                     return new Output(formattedValues);
-//                 } catch (errors) {
-//                     const errorsList = errors.inner.map(error => {
-//                         const { message, path, type, value } = error;
-//                         if (type === "typeError") {
-//                             return new Yup.ValidationError(
-//                                 [`${path} is required`],
-//                                 value,
-//                                 path
-//                             );
-//                         } else {
-//                             return error;
-//                         }
-//                     });
-//                     return {
-//                         ...new Output(),
-//                         errors: formatYupErrors(errorsList)
-//                     };
-//                 }
-//             },
-//             [yupSchema]
-//         );
-//     }
-//
-//     return useForm({
-//         resolver: resolver()
-//     });
-// };
