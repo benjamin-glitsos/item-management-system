6b3a5d251b755140a8d54f4ba3b0eeb4c15f6f86
Benjamin Glitsos <bengyup@gmail.com>
2022-10-01 18:27:04 +1000

    Changed metakey to use a '.' instead of a ':' as a separator

--- database-postgresql/src/functions/gen-metakey.sql
+++ database-postgresql/src/functions/gen-metakey.sql
@@ -1,6 +1,6 @@
 CREATE FUNCTION gen_metakey(namespace text default 'meta')
 RETURNS text AS $$
 BEGIN
-    RETURN CONCAT(namespace, ':', gen_random_uuid());
+    RETURN CONCAT(namespace, '.', gen_random_uuid());
 END;
 $$ LANGUAGE plpgsql;
